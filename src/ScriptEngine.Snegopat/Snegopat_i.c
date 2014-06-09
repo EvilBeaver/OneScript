cd8634d43ceb94391e8b12c9a6047c84a8b8092a
EvilBeaver <ovsiankin.aa@gmail.com>
2014-06-09 12:37:28 +0400

    Исправлена конфигурация сборки Release

--- src/ScriptEngine.Snegopat/Snegopat_i.c
+++ src/ScriptEngine.Snegopat/Snegopat_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Fri Jun 06 17:49:33 2014
+/* at Mon Jun 09 12:34:59 2014
  */
 /* Compiler settings for Snegopat.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
