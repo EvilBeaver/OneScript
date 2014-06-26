e82609cb910cbedb929d02c8ec853241aca22957
EvilBeaver <ovsiankin.aa@gmail.com>
2014-06-26 18:22:07 +0400

    в скрипт сборки добавлен ScriptEngine.Snegopat.dll

--- install/build.cmd
+++ install/build.cmd
@@ -24,6 +24,7 @@ md built
 
 copy "%SolutionPath%\TestApp\bin\x86\Release\ScriptEngine.dll" built\ScriptEngine.dll
 copy "%SolutionPath%\TestApp\bin\x86\Release\ScriptEngine.HostedScript.dll" built\ScriptEngine.HostedScript.dll
+copy "%SolutionPath%\Release\ScriptEngine.Snegopat.dll" built\ScriptEngine.Snegopat.dll
 copy "%SolutionPath%\TestApp\bin\x86\Release\TestApp.exe" built\TestApp.exe
 copy "%SolutionPath%\TestApp\bin\x86\Release\ICSharpCode.AvalonEdit.dll" built\ICSharpCode.AvalonEdit.dll
 copy "%SolutionPath%\oscript\bin\x86\Release\oscript.exe" built\oscript.exe
