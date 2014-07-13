5501e1de2e422191f9a9b072168881f99ad48dbd
EvilBeaver <ovsiankin.aa@gmail.com>
2014-07-13 18:07:11 +0400

    Загрузка SSF, как самостоятельного аддина Снегопата.

--- src/ScriptEngine.Snegopat/ScriptDrivenAddin.cpp
+++ src/ScriptEngine.Snegopat/ScriptDrivenAddin.cpp
@@ -3,14 +3,28 @@
 
 
 ScriptDrivenAddin::ScriptDrivenAddin(LoadedModuleHandle module) 
-	: ScriptDrivenObject(module)
+	: ScriptDrivenObject(module, true)
 {
 	m_marshalledReference = gcnew EventCallableSDO(this, module);
+	m_valuesByIndex = gcnew List<IValue^>();
+	m_names = gcnew Dictionary<String^, int>(StringComparer::InvariantCultureIgnoreCase);
 }
 
 ScriptDrivenAddin::~ScriptDrivenAddin()
 {
-
+	if(m_valuesByIndex != nullptr)
+	{
+		for (int i = 0; i < m_valuesByIndex->Count; i++)
+		{
+			if(Marshal::IsComObject(m_valuesByIndex[i]))
+			{
+				Marshal::ReleaseComObject(m_valuesByIndex[i]);
+			}
+		}
+		m_valuesByIndex->Clear();
+		m_names->Clear();
+		m_valuesByIndex = nullptr;
+	}
 }
 
 Object^ ScriptDrivenAddin::UnderlyingObject::get()
@@ -18,14 +32,27 @@ Object^ ScriptDrivenAddin::UnderlyingObject::get()
 	return m_marshalledReference;
 }
 
+void ScriptDrivenAddin::AddProperty(String^ name, IValue^ value)
+{
+	int newIndex = m_valuesByIndex->Count;
+	m_valuesByIndex->Add(value);
+	m_names->Add(name, newIndex);
+}
+
 int ScriptDrivenAddin::FindOwnProperty(String^ name)
 {
-	if(name->ToLower() == m_thisName)
+	int index = -1;
+	if(m_names->TryGetValue(name, index))
 	{
-		return 0;
+		return index;
 	}
 	else
 	{
 		return -1;
 	}
+}
+
+IValue^ ScriptDrivenAddin::GetOwnPropValue(int index)
+{
+	return m_valuesByIndex[index];
 }
