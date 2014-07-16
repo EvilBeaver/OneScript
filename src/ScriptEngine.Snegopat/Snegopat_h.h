c7abe30235d5575e4cf6e1754c4a0b6bc1148efc
EvilBeaver <ovsiankin.aa@gmail.com>
2014-07-16 18:03:26 +0400

    Для сборки Release удалено автоматическое увеличение номера версии.

--- src/ScriptEngine.Snegopat/Snegopat_h.h
+++ src/ScriptEngine.Snegopat/Snegopat_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Tue Jul 15 09:59:40 2014
+/* at Wed Jul 16 18:01:36 2014
  */
 /* Compiler settings for Snegopat.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
