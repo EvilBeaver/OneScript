49ebdc182be08cca25b3e75b7f1b8f638922ddd2
EvilBeaver <ovsiankin.aa@gmail.com>
2014-07-24 17:36:50 +0400

    Основные интерфейсы

---
+++ src/OneScript/Core/IRuntimeContextInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneScript.Core
+{
+    public interface IRuntimeContextInstance
+    {
+        bool IsIndexed { get; }
+        int GetPropCount();
+        int FindProperty(string name);
+        string GetPropertyName(int index);
+        IValue GetIndexedValue(IValue index);
+        void SetIndexedValue(IValue index, IValue newValue);
+        IValue GetPropertyValue(int index);
+        void SetPropertyValue(int index, IValue newValue);
+        bool DynamicMethodSignatures { get; }
+        int GetMethodsCount();
+        int FindMethod(string name);
+        string GetMethodName(int index);
+        bool HasReturnValue(int index);
+        int GetParametersCount(int index);
+        bool GetDefaultValue(int methodIndex, int paramIndex, out IValue defaultValue);
+        void CallAsProcedure(int index, IValue[] args);
+        IValue CallAsFunction(int index, IValue[] args);
+    }
+}
---
+++ src/OneScript/Core/IValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneScript.Core
+{
+    public interface IValue
+    {
+        DataType Type { get; }
+        double AsNumber();
+        string AsString();
+        DateTime AsDate();
+        bool AsBoolean();
+        IRuntimeContextInstance AsObject();
+    }
+}
