a5fb3ebecb28db599767758c4cdd576253b5d8e2
EvilBeaver <ovsiankin.aa@gmail.com>
2014-10-22 20:13:30 +0400

    Начал работу над ЧтениемXML

---
+++ src/ScriptEngine.HostedScript/Library/XmlReaderBase.cs
@@ -0,0 +1,47 @@
+﻿using ScriptEngine.Machine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace ScriptEngine.HostedScript.Library
+{
+    public class XmlReaderBase : IDisposable
+    {
+        XmlReader _reader;
+
+        public void OpenFile(string path)
+        {
+            if (_reader != null)
+                throw new RuntimeException("Поток XML уже открыт");
+            var textInput = new StreamReader(path);
+            _reader = XmlReader.Create(textInput);
+        }
+
+        public void SetString(string content)
+        {
+            if (_reader != null)
+                throw new RuntimeException("Поток XML уже открыт");
+
+            var textInput = new StringReader(content);
+            _reader = XmlReader.Create(textInput);
+        }
+
+        private void CheckIfOpen()
+        {
+            if (_reader == null)
+                throw new RuntimeException("Файл не открыт");
+        }
+
+        public void Dispose()
+        {
+            if (_reader != null)
+            {
+                _reader.Close();
+                _reader = null;
+            }
+        }
+    }
+}
