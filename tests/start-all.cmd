f2e539ff18d4227dd1d691397ba1a5d83fb30a47
Артур Аюханов aka artbear <aartbear@gmail.com>
2014-11-06 21:20:38 +0300

    Убрал ненужные комменты #65

--- tests/start-all.cmd
+++ tests/start-all.cmd
@@ -4,10 +4,6 @@ setlocal
 set pathdir=%~dp0
 set success=1
 
-rem echo сами тесты %CD%
-rem echo скрипты тестирования %pathdir%
-rem echo "%pathdir%\..\install\built\oscript.exe" %pathdir%\start.os -run %1 %2 %3 %4 %5
-
 for %%I in (*.os) do (
 if NOT "%%~nI"=="start" (
 if NOT "%%~nI"=="testrunner" (
@@ -17,12 +13,7 @@ if NOT "%%~nI"=="testrunner" (
 
 	@call "%pathdir%\..\install\built\oscript.exe" %pathdir%\start.os -run %%I %1 %2 %3 %4 %5
 
-	rem echo %ERRORLEVEL%
 	if NOT %ERRORLEVEL%==0 set success=%ERRORLEVEL%
-	rem set success=%ERRORLEVEL%
-
-	rem echo success = %success%
-	rem GOTO bad_exit
 )
 )
 )
@@ -31,8 +22,6 @@ if NOT "%success%"=="0" GOTO bad_exit
 
 
 :success_exit
-rem echo Успешно
-rem pause
 
 exit /B 0
 
