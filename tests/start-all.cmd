981b9dccc3ddffceea9cd0a99eaf0e3d7da2ed1a
Артур Аюханов aka artbear <aartbear@gmail.com>
2014-11-27 20:43:05 +0300

    Запуск всех тестов из каталога #65 - ВАЖНО считается, что OneScript установлен по пути C:\Program Files (x86)\OneScript\oscript.exe

--- tests/start-all.cmd
+++ tests/start-all.cmd
@@ -4,6 +4,10 @@ setlocal
 set pathdir=%~dp0
 set success=1
 
+set logfile=failtests.log
+del /Q /F %logfile%
+echo . > %logfile%
+
 for %%I in (*.os) do (
 if NOT "%%~nI"=="start" (
 if NOT "%%~nI"=="testrunner" (
@@ -11,9 +15,13 @@ if NOT "%%~nI"=="testrunner" (
 	echo - файл теста -   %%I
 	echo ---
 
-	@call "%pathdir%\..\install\built\oscript.exe" "%pathdir%\start.os" -run %%I %1 %2 %3 %4 %5
+	rem @call "%pathdir%\..\install\built\oscript.exe" "%pathdir%\start.os" -run %%I %1 %2 %3 %4 %5
+	@call "%ProgramFiles(x86)%\OneScript\oscript.exe" "%pathdir%\start.os" -run %%I %1 %2 %3 %4 %5
 
-	if NOT %ERRORLEVEL%==0 set success=%ERRORLEVEL%
+	if NOT %ERRORLEVEL%==0 (
+		set success=%ERRORLEVEL%
+		echo        Упал тест "%%~nI" >> %logfile%
+	)
 )
 )
 )
@@ -30,6 +38,8 @@ if "-1"=="%success%" GOTO success_exit
 echo .
 echo Несколько тестов упали
 echo Неудача.  Красная полоса
+echo    Упавщие тесты:
+type %logfile%
 
 if ".%1"=="." pause
 exit /B 1
