d8b272b7c9451b92d587509b118b73fbe9cc33aa
Артур Аюханов aka artbear <aartbear@gmail.com>
2014-12-04 00:24:09 +0400

    .

--- tests/start-all.cmd
+++ tests/start-all.cmd
@@ -16,7 +16,7 @@ if NOT "%%~nI"=="testrunner" (
 	echo - δ ©« β₯αβ  -   %%I
 	echo ---
 
-	@call %1 "%pathdir%\start.os" -run %%I %2 %3 %4 %5
+	call %1 "%pathdir%\start.os" -run %%I %2 %3 %4 %5
 
 	rem @call "%pathdir%\..\install\built\oscript.exe" "%pathdir%\start.os" -run %%I %1 %2 %3 %4 %5
 	rem @call "%ProgramFiles(x86)%\OneScript\oscript.exe" "%pathdir%\start.os" -run %%I %1 %2 %3 %4 %5
