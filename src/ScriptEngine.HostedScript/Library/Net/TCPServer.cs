159921a110b9ae66009d68e4ba8870cefa9dccd7
EvilBeaver <ovsiankin.aa@gmail.com>
2015-01-02 17:51:46 +0300

    Задел по классу сокета

---
+++ src/ScriptEngine.HostedScript/Library/Net/TCPServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library.Net
+{
+    public class TCPServer : AutoContext<TCPServer>
+    {
+        private TcpListener _listener;
+
+        public TCPServer(int port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public void Listen(string handler)
+        {
+            
+        }
+
+        [ScriptConstructor]
+        public static TCPServer ConstructByPort(IValue port)
+        {
+            return new TCPServer((int)port.AsNumber());
+        }
+    }
+}
