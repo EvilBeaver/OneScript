a2d0e811099a85b168dda350942921853de6a46a
EvilBeaver <ovsiankin.aa@gmail.com>
2015-02-05 11:35:03 +0300

    Частичная реализация ЧтениеZipФайла для срочных личных нужд. #110

---
+++ src/ScriptEngine.HostedScript/Library/Zip/ZipReader.cs
@@ -0,0 +1,47 @@
+﻿using Ionic.Zip;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptEngine.HostedScript.Library.Zip
+{
+    [ContextClass("ЧтениеZipФайла", "ZipFileReader")]
+    public class ZipReader : AutoContext<ZipReader>
+    {
+        ZipFile _zip;
+
+        public ZipReader(string filename)
+        {
+            _zip = ZipFile.Read(filename);
+        }
+
+        [ContextMethod("ИзвлечьВсе","ExtractAll")]
+        public void ExtractAll(string where, SelfAwareEnumValue<ZipRestoreFilePathsModeEnum> restorePaths = null)
+        {
+            _zip.FlattenFoldersOnExtract = FlattenPathsOnExtraction(restorePaths);
+            _zip.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+            _zip.ExtractAll(where);
+        }
+
+        private static bool FlattenPathsOnExtraction(SelfAwareEnumValue<ZipRestoreFilePathsModeEnum> restorePaths)
+        {
+            bool flattenFlag = false;
+            if (restorePaths != null)
+            {
+                var zipEnum = (ZipRestoreFilePathsModeEnum)restorePaths.Owner;
+                flattenFlag = restorePaths == zipEnum.DoNotRestore;
+            }
+
+            return flattenFlag;
+        }
+
+        [ScriptConstructor]
+        public static ZipReader ConstructByName(IValue filename)
+        {
+            return new ZipReader(filename.AsString());
+        }
+    }
+}
---
+++ src/ScriptEngine.HostedScript/Library/Zip/ZipRestoreFilePathsModeEnum.cs
@@ -0,0 +1,51 @@
+﻿using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptEngine.HostedScript.Library.Zip
+{
+    [SystemEnum("РежимВосстановленияПутейФайловZIP", "ZIPRestoreFilePathsMode")]
+    public class ZipRestoreFilePathsModeEnum : EnumerationContext
+    {
+        private const string RESTORE_PATHS_NAME = "Восстанавливать";
+        private const string DONT_RESTORE_PATHS_NAME = "НеВосстанавливать";
+
+        private ZipRestoreFilePathsModeEnum(TypeDescriptor typeRepresentation, TypeDescriptor valuesType)
+            : base(typeRepresentation, valuesType)
+        {
+        }
+
+        public SelfAwareEnumValue<ZipRestoreFilePathsModeEnum> Restore
+        {
+            get
+            {
+                return (SelfAwareEnumValue<ZipRestoreFilePathsModeEnum>)this[RESTORE_PATHS_NAME];
+            }
+        }
+
+        public SelfAwareEnumValue<ZipRestoreFilePathsModeEnum> DoNotRestore
+        {
+            get
+            {
+                return (SelfAwareEnumValue<ZipRestoreFilePathsModeEnum>)this[DONT_RESTORE_PATHS_NAME];
+            }
+        }
+
+        public static ZipRestoreFilePathsModeEnum CreateInstance()
+        {
+            ZipRestoreFilePathsModeEnum instance;
+            var type = TypeManager.RegisterType("ПеречислениеРежимВосстановленияПутейФайловZIPXML", typeof(ZipRestoreFilePathsModeEnum));
+            var enumValueType = TypeManager.RegisterType("РежимВосстановленияПутейФайловZIP", typeof(SelfAwareEnumValue<ZipRestoreFilePathsModeEnum>));
+
+            instance = new ZipRestoreFilePathsModeEnum(type, enumValueType);
+
+            instance.AddValue(RESTORE_PATHS_NAME, new SelfAwareEnumValue<ZipRestoreFilePathsModeEnum>(instance, 0));
+            instance.AddValue(DONT_RESTORE_PATHS_NAME, new SelfAwareEnumValue<ZipRestoreFilePathsModeEnum>(instance, 1));
+
+            return instance;
+        }
+    }
+}
