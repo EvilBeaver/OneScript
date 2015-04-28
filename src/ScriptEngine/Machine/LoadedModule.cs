37d016d48a10222f1f02db4f2bf36b581f478b1b
EvilBeaver <ovsiankin.aa@gmail.com>
2015-04-28 14:14:40 +0300

    fixed #124: Применена лицензия Mozilla Public License 2.0

--- src/ScriptEngine/Machine/LoadedModule.cs
+++ src/ScriptEngine/Machine/LoadedModule.cs
@@ -1,4 +1,10 @@
-﻿using ScriptEngine.Compiler;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using ScriptEngine.Compiler;
 using ScriptEngine.Environment;
 using System;
 using System.Collections.Generic;
