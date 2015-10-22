bcd73dc918b1baa7068b5ad239d032b28160eccf
EvilBeaver <ovsiankin.aa@gmail.com>
2015-10-22 14:25:54 +0300

    Во избежание путаницы, переименовал методы, получающие данные по доступным в скрипте собственным (не скриптовым) методам объекта.

--- src/ScriptEngine/Machine/Contexts/ScriptDrivenObject.cs
+++ src/ScriptEngine/Machine/Contexts/ScriptDrivenObject.cs
@@ -52,8 +52,8 @@ namespace ScriptEngine.Machine.Contexts
         public void InitOwnData()
         {
             
-            VARIABLE_COUNT = GetVariableCount();
-            METHOD_COUNT = GetMethodCount();
+            VARIABLE_COUNT = GetOwnVariableCount();
+            METHOD_COUNT = GetOwnMethodCount();
 
             int stateSize = VARIABLE_COUNT + _module.VariableFrameSize;
             _state = new IVariable[stateSize];
@@ -78,8 +78,8 @@ namespace ScriptEngine.Machine.Contexts
             }
         }
 
-        protected abstract int GetVariableCount();
-        protected abstract int GetMethodCount();
+        protected abstract int GetOwnVariableCount();
+        protected abstract int GetOwnMethodCount();
         protected abstract void UpdateState();
         
         public bool MethodDefinedInScript(int index)
