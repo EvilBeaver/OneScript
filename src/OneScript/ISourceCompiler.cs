bf69e726f440b1b355ba99c73b464dddda961f6d
EvilBeaver <ovsiankin.aa@gmail.com>
2015-10-27 12:04:13 +0300

    Набор директив вынесен в интерфейс компилятора

--- src/OneScript/ISourceCompiler.cs
+++ src/OneScript/ISourceCompiler.cs
@@ -4,7 +4,11 @@ namespace OneScript
     public interface ISourceCompiler
     {
         ILoadedModule Compile(IScriptSource moduleSource);
+        
         void InjectObject(OneScript.Core.IRuntimeContextInstance context);
+        
         void InjectSymbol(string name, OneScript.Core.IValue value);
+        
+        PreprocessorDirectivesSet PreprocessorDirectives { get; }
     }
 }
