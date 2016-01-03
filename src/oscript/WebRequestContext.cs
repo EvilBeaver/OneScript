d060a7201fec3f6b1c0359f2ec1f9d1506065c4e
dmpas <dmpas@dmpas.ru>
2016-01-03 22:54:30 +0300

    В интерфейсе класса ВебЗапрос убрано различие между GET и POST запросами.

--- src/oscript/WebRequestContext.cs
+++ src/oscript/WebRequestContext.cs
@@ -22,7 +22,6 @@ namespace oscript
     public class WebRequestContext : AutoContext<WebRequestContext>
     {
         MapImpl _environmentVars = new MapImpl();
-        MapImpl _get = new MapImpl();
         PostRequestData _post;
         byte[] _post_raw = null;
 
@@ -82,93 +81,44 @@ namespace oscript
 
         private void FillGetMap(string get)
         {
-            ParseFormData(get, _get);
+            _post = new PostRequestData(get);
         }
 
-        private void ParseFormData(string data, MapImpl map)
-        {
-            var pairs = data.Split('&');
-            foreach (var pair in pairs)
-            {
-                var nameVal = pair.Split(new Char[] { '=' }, 2);
-                if (nameVal.Length == 2)
-                {
-                    IValue key = ValueFactory.Create(nameVal[0]);
-                    IValue val = ValueFactory.Create(Decode(nameVal[1]));
-                    map.Insert(key, val);
-                }
-                else if (pair.Length > 0)
-                {
-                    IValue val = ValueFactory.Create(Decode(pair));
-                    map.Insert(val, ValueFactory.Create());
-                }
-            }
-        }
-
-        private static string Decode(string p)
-        {
-            byte[] bytes = new byte[p.Length];
-
-            int j = 0;
-            for (int i = 0; i < p.Length; i++, j++)
-            {
-                if (p[i] == '+')
-                    bytes[j] = 0x20;
-                else if (p[i] == '%')
-                {
-                    bytes[j] = ByteFromHEX(p, ref i);
-                }
-                else
-                    bytes[j] = (byte)p[i];
-            }
-
-            return Encoding.UTF8.GetString(bytes, 0, j);
-
-        }
-
-        private static byte ByteFromHEX(string pattern, ref int index)
-        {
-            System.Diagnostics.Debug.Assert(pattern[index] == '%');
-
-            index++;
-            int major = 0;
-            int minor = 0;
-            for (int i = 0; i < 2; i++)
-            {
-                int code = (int)pattern[index];
-                if (code >= 48 && code <= 57)
-                    code = code - 48;
-                else if (code >= 65 && code <= 70)
-                    code = code - 55;
-
-                if (i == 0)
-                {
-                    major = code;
-                    index++;
-                }
-                else
-                    minor = code;
-            }
-
-            return (byte)(major * 16 + minor);
-
-        }
-
+        // TODO: пометить Deprecated
         [ContextProperty("GET")]
         public IValue GET
         {
             get
             {
-                return _get;
+                return _post.Params;
             }
         }
 
+        // TODO: пометить Deprecated
         [ContextProperty("POST")]
         public IValue POST
         {
             get
             {
                 return _post;
+            }
+        }
+
+        [ContextProperty("Параметры", "Params")]
+        public MapImpl Params
+        {
+            get
+            {
+                return _post.Params;
+            }
+        }
+
+        [ContextProperty("Файлы", "Files")]
+        public ArrayImpl Files
+        {
+            get
+            {
+                return _post.Files;
             }
         }
 
