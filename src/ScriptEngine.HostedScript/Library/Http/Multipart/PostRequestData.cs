454449ccb341dd26b5f63e47225360b0f16ae0ff
Сергѣй Батанов <dmpas@dmpas.ru>
2016-01-09 22:16:41 +0300

    Переименовал файл, запустил форматирование.

---
+++ src/ScriptEngine.HostedScript/Library/Http/Multipart/PostRequestData.cs
@@ -0,0 +1,125 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using HttpMultipartParser;
+
+namespace ScriptEngine.HostedScript.Library.Http.Multipart
+{
+    [ContextClass("ДанныеPOSTЗапроса", "PostRequestData")]
+    public class PostRequestData : AutoContext<PostRequestData>
+    {
+
+        private MapImpl _params = new MapImpl();
+        private ArrayImpl _files = new ArrayImpl(); // TODO: MapImpl для файлов POST-запроса
+
+        public PostRequestData(byte []buffer, string boundary)
+        {
+            using (var stdin = new MemoryStream(buffer))
+            {
+                var parser = new MultipartFormDataParser(stdin, boundary, Encoding.UTF8);
+                foreach (var param in parser.Parameters)
+                {
+                    _params.Insert(ValueFactory.Create(param.Name), ValueFactory.Create(param.Data));
+                }
+
+                foreach (var file in parser.Files)
+                {
+                    _files.Add(ValueFactory.Create(new PostFileDescription(file)));
+                }
+            }
+        }
+
+        public PostRequestData(string data)
+        {
+            var pairs = data.Split('&');
+            foreach (var pair in pairs)
+            {
+                var nameVal = pair.Split(new Char[] { '=' }, 2);
+                if (nameVal.Length == 2)
+                {
+                    IValue key = ValueFactory.Create(nameVal[0]);
+                    IValue val = ValueFactory.Create(Decode(nameVal[1]));
+                    _params.Insert(key, val);
+                }
+                else if (pair.Length > 0)
+                {
+                    IValue val = ValueFactory.Create(Decode(pair));
+                    _params.Insert(val, ValueFactory.Create());
+                }
+            }
+        }
+
+        private static string Decode(string p)
+        {
+            byte[] bytes = new byte[p.Length];
+
+            int j = 0;
+            for (int i = 0; i < p.Length; i++, j++)
+            {
+                if (p[i] == '+')
+                    bytes[j] = 0x20;
+                else if (p[i] == '%')
+                {
+                    bytes[j] = ByteFromHEX(p, ref i);
+                }
+                else
+                    bytes[j] = (byte)p[i];
+            }
+
+            return Encoding.UTF8.GetString(bytes, 0, j);
+
+        }
+
+        private static byte ByteFromHEX(string pattern, ref int index)
+        {
+            System.Diagnostics.Debug.Assert(pattern[index] == '%');
+
+            index++;
+            int major = 0;
+            int minor = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                int code = (int)pattern[index];
+                if (code >= 48 && code <= 57)
+                    code = code - 48;
+                else if (code >= 65 && code <= 70)
+                    code = code - 55;
+
+                if (i == 0)
+                {
+                    major = code;
+                    index++;
+                }
+                else
+                    minor = code;
+            }
+
+            return (byte)(major * 16 + minor);
+
+        }
+
+        [ContextProperty("Параметры", "Params")]
+        public MapImpl Params
+        { get { return _params; } }
+
+        [ContextProperty("Файлы", "Files")]
+        public ArrayImpl Files
+        { get { return _files; } }
+
+        [ScriptConstructor(Name="Из двоичных данных")]
+        public static PostRequestData Constructor(BinaryDataContext data, IValue boundary)
+        {
+            return new PostRequestData(data.Buffer, boundary.ToString());
+        }
+    }
+}
--- src/oscript/WebRequestContext.cs
+++ src/oscript/WebRequestContext.cs
@@ -18,133 +18,117 @@ using ScriptEngine.HostedScript.Library.Http.Multipart;
 
 namespace oscript
 {
-    [ContextClass("ВебЗапрос", "WebRequest")]
+    [ContextClass ("ВебЗапрос", "WebRequest")]
     public class WebRequestContext : AutoContext<WebRequestContext>
     {
-        MapImpl _environmentVars = new MapImpl();
+        MapImpl _environmentVars = new MapImpl ();
         PostRequestData _post;
         byte[] _post_raw = null;
 
-        public WebRequestContext()
+        public WebRequestContext ()
         {
-            string get = Environment.GetEnvironmentVariable("QUERY_STRING");
-            if (get != null)
-            {
-                FillGetMap(get);
+            string get = Environment.GetEnvironmentVariable ("QUERY_STRING");
+            if (get != null) {
+                FillGetMap (get);
             }
 
-            ProcessPostData();
+            ProcessPostData ();
 
-            FillEnvironmentVars();
+            FillEnvironmentVars ();
 
         }
 
-        private void ProcessPostData()
+        private void ProcessPostData ()
         {
-            var contentLen = Environment.GetEnvironmentVariable("CONTENT_LENGTH");
+            var contentLen = Environment.GetEnvironmentVariable ("CONTENT_LENGTH");
             if (contentLen == null)
                 return;
 
-            int len = Int32.Parse(contentLen);
+            int len = Int32.Parse (contentLen);
             if (len == 0)
                 return;
 
             // THINK: правильно ли хранить всегда весь запрос в памяти???
             _post_raw = new byte[len];
-            using (var stdin = Console.OpenStandardInput())
-            {
-                stdin.Read(_post_raw, 0, len);
+            using (var stdin = Console.OpenStandardInput ()) {
+                stdin.Read (_post_raw, 0, len);
             }
 
-            var type = Environment.GetEnvironmentVariable("CONTENT_TYPE");
-            if (type != null && type.StartsWith("multipart/"))
-            {
-                var boundary = type.Substring(type.IndexOf('=') + 1);
-                _post = new PostRequestData(_post_raw, boundary);
-            }
-            else
-            {
-                _post = new PostRequestData(Encoding.UTF8.GetString(_post_raw));
+            var type = Environment.GetEnvironmentVariable ("CONTENT_TYPE");
+            if (type != null && type.StartsWith ("multipart/")) {
+                var boundary = type.Substring (type.IndexOf ('=') + 1);
+                _post = new PostRequestData (_post_raw, boundary);
+            } else {
+                _post = new PostRequestData (Encoding.UTF8.GetString (_post_raw));
             }
 
         }
 
-        private void FillEnvironmentVars()
+        private void FillEnvironmentVars ()
         {
-            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables())
-            {
-                _environmentVars.Insert(
-                    ValueFactory.Create((string)item.Key),
-                    ValueFactory.Create((string)item.Value));
+            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables()) {
+                _environmentVars.Insert (
+                    ValueFactory.Create ((string)item.Key),
+                    ValueFactory.Create ((string)item.Value));
             }
         }
 
-        private void FillGetMap(string get)
+        private void FillGetMap (string get)
         {
-            _post = new PostRequestData(get);
+            _post = new PostRequestData (get);
         }
 
-        // TODO: пометить Deprecated
-        [ContextProperty("GET")]
-        public IValue GET
-        {
-            get
-            {
+        [Obsolete ()]
+        [ContextProperty ("GET")]
+        public IValue GET {
+            get {
                 return _post.Params;
             }
         }
 
-        // TODO: пометить Deprecated
-        [ContextProperty("POST")]
-        public IValue POST
-        {
-            get
-            {
+        [Obsolete ()]
+        [ContextProperty ("POST")]
+        public IValue POST {
+            get {
                 return _post;
             }
         }
 
-        [ContextProperty("Параметры", "Params")]
-        public MapImpl Params
-        {
-            get
-            {
+        [ContextProperty ("Параметры", "Params")]
+        public MapImpl Params {
+            get {
                 return _post.Params;
             }
         }
 
-        [ContextProperty("Файлы", "Files")]
-        public ArrayImpl Files
-        {
-            get
-            {
+        [ContextProperty ("Файлы", "Files")]
+        public ArrayImpl Files {
+            get {
                 return _post.Files;
             }
         }
 
-        [ContextProperty("ENV")]
-        public IValue ENV
-        {
-            get
-            {
+        [ContextProperty ("ENV")]
+        public IValue ENV {
+            get {
                 return _environmentVars;
             }
         }
 
-        [ContextMethod("ПолучитьТелоКакДвоичныеДанные", "GetBodyAsBinaryData")]
-        public BinaryDataContext GetBodyAsBinaryData()
+        [ContextMethod ("ПолучитьТелоКакДвоичныеДанные", "GetBodyAsBinaryData")]
+        public BinaryDataContext GetBodyAsBinaryData ()
         {
-            return new BinaryDataContext(_post_raw);
+            return new BinaryDataContext (_post_raw);
         }
 
-        [ContextMethod("ПолучитьТелоКакСтроку", "GetBodyAsString")]
-        public string GetBodyAsString(IValue encoding = null)
+        [ContextMethod ("ПолучитьТелоКакСтроку", "GetBodyAsString")]
+        public string GetBodyAsString (IValue encoding = null)
         {
-            Encoding enc = (encoding == null || ValueFactory.Create().Equals(encoding))
-                    ? new UTF8Encoding(false)
-                    : TextEncodingEnum.GetEncoding(encoding);
+            Encoding enc = (encoding == null || ValueFactory.Create ().Equals (encoding))
+                    ? new UTF8Encoding (false)
+                    : TextEncodingEnum.GetEncoding (encoding);
 
-            return enc.GetString(_post_raw);
+            return enc.GetString (_post_raw);
         }
     }
 }
