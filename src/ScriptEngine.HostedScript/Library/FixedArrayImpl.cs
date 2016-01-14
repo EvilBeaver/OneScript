3f6b51dc187a9dcd66496d22b73533dbff095b53
Sergey Batanov <dmpas@dmpas.ru>
2016-01-14 11:04:21 +0300

    Добавлены классы ФиксированноеСоответствие и ФиксированныйМассив

---
+++ src/ScriptEngine.HostedScript/Library/FixedArrayImpl.cs
@@ -0,0 +1,109 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library
+{
+    [ContextClass("ФиксированныйМассив", "FixedArray")]
+    public class FixedArrayImpl : AutoContext<FixedArrayImpl>, ICollectionContext
+    {
+        private readonly ArrayImpl _array;
+
+        public FixedArrayImpl(ArrayImpl source)
+        {
+            _array = new ArrayImpl();
+            foreach (var Value in source)
+            {
+                _array.Add(Value);
+            }
+        }
+
+        public override bool IsIndexed
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override IValue GetIndexedValue(IValue index)
+        {
+            return _array.GetIndexedValue(index);
+        }
+
+        public override bool IsPropReadable(int propNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsPropWritable(int propNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region ICollectionContext Members
+
+        [ContextMethod("Количество", "Count")]
+        public int Count()
+        {
+            return _array.Count();
+        }
+
+        public CollectionEnumerator GetManagedIterator()
+        {
+            return new CollectionEnumerator(GetEnumerator());
+        }
+
+        #endregion
+
+        #region IEnumerable<IRuntimeContextInstance> Members
+
+        public IEnumerator<IValue> GetEnumerator()
+        {
+            return _array.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        [ContextMethod("Найти", "Find")]
+        public IValue Find(IValue what)
+        {
+            return _array.Find(what);
+        }
+
+        [ContextMethod("ВГраница", "UBound")]
+        public int UpperBound()
+        {
+            return _array.UpperBound();
+        }
+
+        [ContextMethod("Получить", "Get")]
+        public IValue Get(int index)
+        {
+            return _array.Get(index);
+        }
+
+        [ScriptConstructor(Name = "На основании обычного массива")]
+        public static IRuntimeContextInstance Constructor(IValue source)
+        {
+            ArrayImpl RawSource = source.GetRawValue() as ArrayImpl;
+            return new FixedArrayImpl(RawSource);
+        }
+    }
+}
---
+++ src/ScriptEngine.HostedScript/Library/FixedMapImpl.cs
@@ -0,0 +1,98 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using System.Collections.Generic;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library
+{
+    [ContextClass("ФиксированноеСоответствие", "FixedMap")]
+    public class FixedMapImpl : AutoContext<FixedMapImpl>, ICollectionContext
+    {
+
+        private readonly MapImpl _map;
+
+        public FixedMapImpl(MapImpl source)
+        {
+            _map = new MapImpl();
+            foreach (KeyAndValueImpl KV in source)
+            {
+                _map.Insert(KV.Key, KV.Value);
+            }
+        }
+
+        public override bool IsIndexed
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override IValue GetIndexedValue(IValue index)
+        {
+            return _map.GetIndexedValue(index);
+        }
+
+        public override bool IsPropReadable(int propNum)
+        {
+            return _map.IsPropReadable(propNum);
+        }
+
+        public override bool IsPropWritable(int propNum)
+        {
+            return _map.IsPropWritable(propNum);
+        }
+        
+        #region ICollectionContext Members
+
+        [ContextMethod("Получить", "Get")]
+        public IValue Retrieve(IValue key)
+        {
+            return GetIndexedValue(key);
+        }
+
+        [ContextMethod("Количество", "Count")]
+        public int Count()
+        {
+            return _map.Count();
+        }
+
+        public CollectionEnumerator GetManagedIterator()
+        {
+            return _map.GetManagedIterator();
+        }
+
+        #endregion
+
+        #region IEnumerable<IValue> Members
+
+        public IEnumerator<IValue> GetEnumerator()
+        {
+            return _map.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor(IValue source)
+        {
+            MapImpl RawSource = source.GetRawValue() as MapImpl;
+            return new FixedMapImpl(RawSource);
+        }
+    }
+
+}
