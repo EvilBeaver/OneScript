3043101540c41c194e8f8468e4683ab25747156e
Сергѣй Батанов <dmpas@dmpas.ru>
2016-01-26 00:39:05 +0300

    Доработки по упрощению сборки deb-пакета

--- install/builders/deb/build.sh
+++ install/builders/deb/build.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 # TODO: $1
 SRCPATH=/media/
 BINPATH=${SRCPATH}src/oscript/bin/Release/
 DEBBUILDROOT=${SRCPATH}src/oscript/bin/
+BUILDERROOT=${SRCPATH}install/builders/deb/
 
-# Хитрый способ вычленения версии ОдноСкрипта
-VERSION=$(mono ${BINPATH}oscript.exe | head -1 | grep -oE '([[:digit:]]+\.){3,3}[[:digit:]]+')
+VERSION=$(cat ${BINPATH}VERSION)
 PAKNAME=onescript-engine_${VERSION}
-
 
 mkdir ${DEBBUILDROOT}${PAKNAME} 
 mkdir -p ${DEBBUILDROOT}${PAKNAME}/DEBIAN
 mkdir -p ${DEBBUILDROOT}${PAKNAME}/usr/bin
 mkdir -p ${DEBBUILDROOT}${PAKNAME}/usr/lib/oscript
 
-cp ${SRCPATH}install/builders/deb/settings/* ${DEBBUILDROOT}${PAKNAME}/DEBIAN/
-cp ${BINPATH}*.exe ${DEBBUILDROOT}${PAKNAME}/usr/bin 
-cp ${BINPATH}*.dll ${DEBBUILDROOT}${PAKNAME}/usr/bin
+cp ${BUILDERROOT}settings/dirs ${DEBBUILDROOT}${PAKNAME}/DEBIAN/
+cat ${BUILDERROOT}settings/control | sed -r "s/VERSION/$VERSION/g" > ${DEBBUILDROOT}${PAKNAME}/DEBIAN/control
+cp ${BINPATH}*.exe ${DEBBUILDROOT}${PAKNAME}/usr/bin/ 
+cp ${BINPATH}*.dll ${DEBBUILDROOT}${PAKNAME}/usr/bin/
+cp ${BUILDERROOT}oscript ${DEBBUILDROOT}${PAKNAME}/usr/bin/
+cp ${BUILDERROOT}oscript-cgi ${DEBBUILDROOT}${PAKNAME}/usr/bin/
 
 fakeroot dpkg-deb --build ${DEBBUILDROOT}${PAKNAME}
 
 rm -rf ${DEBBUILDROOT}${PAKNAME}
 chmod 777 ${DEBBUILDROOT}${PAKNAME}.deb
 
+####
+#	Тестирование. TODO: Вынести в отдельный контейнер
+####
+
 # проверим установку
 
-dpkg --install ${DEBBUILDROOT}${PAKNAME}.deb && \
-# вывод версии
-	mono /usr/bin/oscript.exe | head -1
+dpkg --force-depends --install ${DEBBUILDROOT}${PAKNAME}.deb && apt-get -f -y install
 
 # запуск тестов
+oscript ${SRCPATH}tests/testrunner.os -runall ${SRCPATH}tests
 
--- install/deb-build.sh
+++ install/deb-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 SRCPATH=/media
+BINPATH=${PWD}/../src/oscript/bin/Release/
+mono ${BINPATH}oscript.exe | head -1 | \
+		grep -oE '([[:digit:]]+\.){3,3}[[:digit:]]+' \
+		> ${BINPATH}VERSION
 
 docker build -t onescript:deb ${PWD}/builders/deb/
 docker run -v ${PWD}/..:${SRCPATH} --name onescript_deb onescript:deb ${SRCPATH}
