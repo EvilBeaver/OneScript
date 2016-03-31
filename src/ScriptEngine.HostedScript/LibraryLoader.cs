69629d88ecf96fc9783b4d54d7b96a23ed6e27cd
EvilBeaver <ovsiankin.aa@gmail.com>
2016-03-31 18:01:42 +0300

    Рефакторинг API подключения скриптов.

--- src/ScriptEngine.HostedScript/LibraryLoader.cs
+++ src/ScriptEngine.HostedScript/LibraryLoader.cs
@@ -247,13 +247,17 @@ namespace ScriptEngine.HostedScript
             {
                 var compiler = _engine.GetCompilerService();
 
+                var source = _engine.Loader.FromFile(script.path);
+                var module = _engine.AttachedScriptsFactory.CreateModuleFromSource(compiler, source, null);
+
                 if(script.asClass)
                 {
-                    _engine.AttachedScriptsFactory.AttachByPath(compiler, script.path, script.identifier);
+                    _engine.AttachedScriptsFactory.LoadAndRegister(script.identifier, module);
                 }
                 else
-                {
-                    var instance = (IValue)_engine.AttachedScriptsFactory.LoadFromPath(compiler, script.path);
+                {                    
+                    var loaded = _engine.LoadModuleImage(module);
+                    var instance = (IValue)_engine.NewObject(loaded);
                     _env.SetGlobalProperty(script.identifier, instance);
                 }
             }
