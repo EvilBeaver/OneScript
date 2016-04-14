18d9fbba2eb24722d8819602e69b1152f56b1b2b
Сергѣй Батанов <dmpas@dmpas.ru>
2016-04-14 22:15:32 +0300

    Deb: Убрана сборка бинарников.

--- install/deb-build.sh
+++ install/deb-build.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+BINPATH=$(cd $1; pwd)
 cd `dirname $0`
 
-SRCPATH=/media
-BINPATH=${PWD}/../src/oscript/bin/Release/
-mono ${BINPATH}oscript.exe | head -1 | \
+if [ -z "$TMP" ] ; then
+	TMP=/tmp
+fi
+
+TMPDIR=$TMP/oscript-deb-builder
+
+mkdir -p $TMPDIR/deb
+mkdir -p $TMPDIR/bin
+mkdir -p $TMPDIR/lib
+
+cp -r $BINPATH/* $TMPDIR/bin
+cp -r ../install/builders/deb/* $TMPDIR/deb
+cp -r ../oscript-library/* $TMPDIR/lib
+
+mono ${BINPATH}/oscript.exe | head -1 | \
 		grep -oE '([[:digit:]]+\.){2}[[:digit:]]+' \
 		> ${BINPATH}VERSION
 
 docker build -t onescript:deb ${PWD}/builders/deb/
-docker run --rm -v ${PWD}/..:${SRCPATH} onescript:deb ${SRCPATH}
+docker run --rm -v ${TMPDIR}:/media onescript:deb 
+
+cp $TMPDIR/bin/*.deb $BINPATH
+rm -rd $TMPDIR
 
