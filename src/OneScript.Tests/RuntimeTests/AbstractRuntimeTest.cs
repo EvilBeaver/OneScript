a5a1b4291a598fb533edf22719e69fbf61bdac8d
EvilBeaver <ovsiankin.aa@gmail.com>
2016-04-15 17:50:16 +0300

    Файл тестов API для абстрактного runtime

---
+++ src/OneScript.Tests/RuntimeTests/AbstractRuntimeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneScript.Runtime;
+using OneScript.Core;
+
+namespace OneScript.Tests.RuntimeTests
+{
+    [TestClass]
+    public class AbstractRuntimeTest
+    {
+        [TestMethod]
+        public void Abstract_Runtime_API_Can_Execute_Simple_Code()
+        {
+            IScriptRuntime rt = new OneScriptRuntime();
+            IScriptSource src = new StringCodeSource("А = 1");
+
+            var module = rt.Compile(src);
+            rt.Execute(module, module.EntryPointName);
+        }
+    }
+}
