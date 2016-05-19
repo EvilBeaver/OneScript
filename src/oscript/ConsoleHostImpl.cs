fef13f150e76caa19bcb4a6b7a44f030465828bc
EvilBeaver <ovsiankin.aa@gmail.com>
2016-05-19 19:03:51 +0300

    Добавил префикс лицензии в новые файлы

--- src/oscript/ConsoleHostImpl.cs
+++ src/oscript/ConsoleHostImpl.cs
@@ -1,9 +1,13 @@
-﻿using ScriptEngine;
-using ScriptEngine.HostedScript;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using ScriptEngine;
+using ScriptEngine.HostedScript;
 
 namespace oscript
 {
