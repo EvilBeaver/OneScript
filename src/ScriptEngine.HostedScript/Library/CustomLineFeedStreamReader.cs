22485d804ea1d1401f5a7942071dbee44f2e4c3b
Sergey Batanov <sergey.batanov@dmpas.ru>
2016-09-19 16:45:12 +0300

    Добавлено экранирование в тесте text-read.os

---
+++ src/ScriptEngine.HostedScript/Library/CustomLineFeedStreamReader.cs
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using System;
+using System.IO;
+using System.Text;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.Collections.Generic;
+
+namespace ScriptEngine.HostedScript.Library
+{
+
+    class CustomLineFeedStreamReader : StreamReader
+    {
+    }
+    
+}
