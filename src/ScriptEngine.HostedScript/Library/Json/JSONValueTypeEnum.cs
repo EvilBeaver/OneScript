1249a79c4b2e2d5cc2dab91f6f3c45f751f710ed
alexkmbk <alexkmbkdr1@gmail.com>
2016-11-11 00:57:21 +0600

    Добавлен и реализован объект ЧтениеJSON с использованием фреймворка Newtonsoft.Json Добавлено перечисление ТипЗначенияJSON Добавлены тесты
    Нерешенные проблемы:
    Значения свойств ТекущаяСтрока и ТекущаяПозиция в некоторых случаях отличаются от значений, которые выдаются платформой 1С: Предприятие на том же самом Json тексте. Пока сильно с этим не разбирался, поскольку эти свойства кажутся не очень критичными для функционала.

---
+++ src/ScriptEngine.HostedScript/Library/Json/JSONValueTypeEnum.cs
@@ -0,0 +1,120 @@
+﻿using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library.Json
+{
+    [SystemEnum("ТипЗначенияJSON", "JSONValueType")]
+    public class JSONValueTypeEnum : EnumerationContext
+    {
+        private JSONValueTypeEnum(TypeDescriptor typeRepresentation, TypeDescriptor valuesType)
+           : base(typeRepresentation, valuesType)
+        {
+        }
+
+        [EnumValue("Null")]
+        public EnumerationValue Null
+        {
+            get
+            {
+                return this["Null"];
+            }
+        }
+
+        [EnumValue("Булево", "Boolean")]
+        public EnumerationValue Boolean
+        {
+            get
+            {
+                return this["Булево"];
+            }
+        }
+
+        [EnumValue("ИмяСвойства", "PropertyName")]
+        public EnumerationValue PropertyName
+        {
+            get
+            {
+                return this["ИмяСвойства"];
+            }
+        }
+
+        [EnumValue("Комментарий", "Comment")]
+        public EnumerationValue Comment
+        {
+            get
+            {
+                return this["Комментарий"];
+            }
+        }
+
+        [EnumValue("КонецМассива", "ArrayEnd")]
+        public EnumerationValue ArrayEnd
+        {
+            get
+            {
+                return this["КонецМассива"];
+            }
+        }
+
+        [EnumValue("КонецОбъекта", "ObjectEnd")]
+        public EnumerationValue ObjectEnd
+        {
+            get
+            {
+                return this["КонецОбъекта"];
+            }
+        }
+
+        [EnumValue("НачалоМассива", "ArrayStart")]
+        public EnumerationValue ArrayStart
+        {
+            get
+            {
+                return this["НачалоМассива"];
+            }
+        }
+
+        [EnumValue("НачалоОбъекта", "ObjectStart")]
+        public EnumerationValue ObjectStart
+        {
+            get
+            {
+                return this["НачалоОбъекта"];
+            }
+        }
+
+        [EnumValue("Ничего", "None")]
+        public EnumerationValue None
+        {
+            get
+            {
+                return this["Ничего"];
+            }
+        }
+
+        [EnumValue("Строка", "String")]
+        public EnumerationValue String
+        {
+            get
+            {
+                return this["Строка"];
+            }
+        }
+
+        [EnumValue("Число", "Number")]
+        public EnumerationValue Number
+        {
+            get
+            {
+                return this["Число"];
+            }
+        }
+
+        public static JSONValueTypeEnum CreateInstance()
+        {
+            return EnumContextHelper.CreateEnumInstance<JSONValueTypeEnum>((t, v) => new JSONValueTypeEnum(t, v));
+        }
+
+    }
+
+}
---
+++ tests/json/test.json
@@ -0,0 +1,13 @@
+{
+   "firstName": "Иван",
+   "lastName": "Иванов",
+   "address": {
+       "streetAddress": "Московское ш., 101, кв.101",
+       "city": "Ленинград",
+       "postalCode": 101101
+   },
+   "phoneNumbers": [
+       "812 123-1234",
+       "916 123-4567"
+   ]
+}
