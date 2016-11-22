2c8acbdac56af97bc3a786c473ee14c7f88652f4
alexkmbk <alexkmbkdr1@gmail.com>
2016-11-22 14:17:24 +0600

    Добавлены объекты: -ЗаписьJSON -ПараметрыЗаписиJSON -ПереносСтрокJSON -ЭкранированиеСимволовJSON
    добавлен тест json_writer.os
    На данный момент тестов по новым объектам явно не достаточно.
    
    Не реализовано:
    - поддержка настройки СимволыВнеBMP (выдается исключение)
    - поддержка настройки ПроверятьСтруктуру (выдается исключение)
    
    Текущие проблемы:
    при включенной настройке ИспользоватьФорматСЭкспонентой в методе ЗаписатьЗначение, полученный формат числа, несколько отличается от того, который выдает платформа.

---
+++ src/ScriptEngine.HostedScript/Library/Json/JSONLineBreak.cs
@@ -0,0 +1,57 @@
+﻿using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library.Json
+{
+    [SystemEnum("ПереносСтрокJSON", "JSONLineBreak")]
+    public class JSONLineBreakEnum : EnumerationContext
+    {
+        private JSONLineBreakEnum(TypeDescriptor typeRepresentation, TypeDescriptor valuesType)
+           : base(typeRepresentation, valuesType)
+        {
+        }
+
+        [EnumValue("Unix")]
+        public EnumerationValue Unix
+        {
+            get
+            {
+                return this["Unix"];
+            }
+        }
+
+        [EnumValue("Windows")]
+        public EnumerationValue Windows
+        {
+            get
+            {
+                return this["Windows"];
+            }
+        }
+
+        [EnumValue("Авто", "Auto")]
+        public EnumerationValue Auto
+        {
+            get
+            {
+                return this["Авто"];
+            }
+        }
+
+        [EnumValue("Нет", "None")]
+        public EnumerationValue None
+        {
+            get
+            {
+                return this["Нет"];
+            }
+        }
+
+        public static JSONLineBreakEnum CreateInstance()
+        {
+            return EnumContextHelper.CreateEnumInstance<JSONLineBreakEnum>((t, v) => new JSONLineBreakEnum(t, v));
+        }
+
+    }
+
+}
