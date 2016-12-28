d83e0044c0e8085627ba9deffa73c6e61a13cb48
Alexey Lustin <alustin@silverbulleters.org>
2016-12-28 22:44:50 +0300

    Добавлен тест обертка и проект NUnit

---
+++ src/NUnitTests/TestRunnerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NUnitTests
+{
+    [TestClass]
+    public class UnitTestWrapper
+    {
+        [TestMethod]
+        public void RunEngineTests()
+        {
+            Equals("", "");
+
+        }
+    }
+}
