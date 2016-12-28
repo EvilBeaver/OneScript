24320cd154c6de357768cd33805205b0ce30cfed
Alexey Lustin <alustin@silverbulleters.org>
2016-12-29 02:22:36 +0300

    приведение в порядок скрипта и игнор файла

--- coverals.io.bat
+++ coverals.io.bat
@@ -2,4 +2,4 @@ echo "Publish coverals io info"
 echo "dont forget set secure repo token ;-) COVERALLS_REPO_TOKEN"
 echo "dont forget use branch and pull request params"
 
-"%CD%\src\packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe" --useRelativePaths --commitBranch %BRANCH% --opencover -i %CD%\src\OpenCover\opencover.xml
+"%CD%\src\packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe" --useRelativePaths --opencover -i %CD%\src\OpenCover\opencover.xml
