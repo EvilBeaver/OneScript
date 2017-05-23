c87c6e6e90d3bb14ccae6eea0537a0edceddb9ac
ret-Phoenix <phoenixusa@yandex.ru>
2017-05-23 23:58:27 +0300

    Начал перевод классов из расширения

--- src/ScriptEngine.HostedScript/Library/SystemEnvironmentContext.cs
+++ src/ScriptEngine.HostedScript/Library/SystemEnvironmentContext.cs
@@ -73,6 +73,83 @@ namespace ScriptEngine.HostedScript.Library
         }
 
         /// <summary>
+        /// Определяет, является ли текущая операционная система 64-разрядной.
+        /// </summary>
+        [ContextProperty("Это64БитнаяОперационнаяСистема")]
+        public bool Is64BitOperatingSystem
+        {
+            get { return System.Environment.Is64BitOperatingSystem; }
+        }
+
+        /// <summary>
+        /// Возвращает число процессоров.
+        /// 32-битовое целое число со знаком, которое возвращает количество процессоров на текущем компьютере. 
+        /// Значение по умолчанию отсутствует. Если текущий компьютер содержит несколько групп процессоров, 
+        /// данное свойство возвращает число логических процессоров, доступных для использования средой CLR
+        /// </summary>
+        [ContextProperty("КоличествоПроцессоров")]
+        public int ProcessorCount
+        {
+            get { return System.Environment.ProcessorCount; }
+        }
+
+        /// <summary>
+        /// Возвращает количество байтов на странице памяти операционной системы
+        /// </summary>
+        [ContextProperty("РазмерСистемнойСтраницы")]
+        public int SystemPageSize
+        {
+            get { return System.Environment.SystemPageSize; }
+        }
+
+        /// <summary>
+        /// Возвращает время, истекшее с момента загрузки системы (в миллисекундах).
+        /// </summary>
+        [ContextProperty("ВремяРаботыСМоментаЗагрузки")]
+        public int TickCount
+        {
+            get { return System.Environment.TickCount; }
+        }
+
+
+        ///// <summary>
+        ///// Список специальных папок
+        ///// </summary>
+        //[ContextProperty("СпециальнаяПапка")]
+        //public IValue SpecialFolder
+        //{
+        //    get
+        //    {
+        //        return new SpecialFolder();
+        //    }
+        //}
+
+        [ContextMethod("ПолучитьПутьПапки")]
+        public string GetFolderPath(IValue folder)
+        {
+            return System.Environment.GetFolderPath((System.Environment.SpecialFolder)folder.AsNumber());
+        }
+
+        /// <summary>
+        /// Возвращает массив строк, содержащий имена логических дисков текущего компьютера.
+        /// </summary>
+        [ContextProperty("ИменаЛогическихДисков")]
+        public ArrayImpl GetLogicalDrives
+        {
+            get
+            {
+                ArrayImpl arr = new ArrayImpl();
+                var data = System.Environment.GetLogicalDrives();
+                foreach (var itm in data)
+                {
+                    arr.Add(ValueFactory.Create(itm));
+                }
+                return arr;
+            }
+        }
+
+
+        /// <summary>
         /// Возвращает соответствие переменных среды. Ключом является имя переменной, а значением - значение переменной
         /// </summary>
         /// <example>
@@ -133,5 +210,56 @@ namespace ScriptEngine.HostedScript.Library
         {
             return new SystemEnvironmentContext();
         }
+    }
+
+    [SystemEnum("СпециальнаяПапка", "SpecialFolder")]
+    class ServiceStartModeEnum : EnumerationContext
+    {
+        private ServiceStartModeEnum(TypeDescriptor typeRepresentation, TypeDescriptor valuesType)
+            : base(typeRepresentation, valuesType)
+        {
+
+        }
+
+        public static ServiceStartModeEnum CreateInstance()
+        {
+            ServiceStartModeEnum instance;
+            var type = TypeManager.RegisterType("ПеречислениеРежимЗапуска", typeof(ServiceStartModeEnum));
+            var enumValueType = TypeManager.RegisterType("РежимЗапуска", typeof(CLREnumValueWrapper<SpecialFolder>));
+
+            instance = new ServiceStartModeEnum(type, enumValueType);
+
+            instance.AddValue("РепозиторийДокументов", "Personal", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.Personal));
+            instance.AddValue("ДанныеПриложений", "ApplicationData", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.ApplicationData));
+            instance.AddValue("ЛокальныйКаталогДанныхПриложений", "LocalApplicationData", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.LocalApplicationData));
+            instance.AddValue("КаталогРабочийСтол", "Desktop", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.Desktop));
+            instance.AddValue("Вручную", "DesktopDirectory", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.DesktopDirectory));
+            instance.AddValue("System", "MyMusic", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.MyMusic));
+            instance.AddValue("System", "MyPictures", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.MyPictures));
+            instance.AddValue("System", "Templates", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.Templates));
+            instance.AddValue("System", "MyVideos", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.MyVideos));
+            instance.AddValue("System", "CommonTemplates", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.CommonTemplates));
+            instance.AddValue("System", "UserProfile", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.UserProfile));
+            instance.AddValue("System", "CommonApplicationData", new CLREnumValueWrapper<SpecialFolder>(instance, SpecialFolder.CommonApplicationData));
+
+            return instance;
+        }
+    }
+
+    public enum SpecialFolder
+    {
+        Personal = 0x05,
+        ApplicationData = 0x1a,
+        LocalApplicationData = 0x1c,
+        Desktop = 0x00,
+        DesktopDirectory = 0x10,
+        MyMusic = 0x0d,
+        MyPictures = 0x27,
+        Templates = 0x15,
+        MyVideos = 0x0e,
+        CommonTemplates = 0x2d,
+        Fonts = 0x14,
+        UserProfile = 0x28,
+        CommonApplicationData = 0x23
     }
 }
