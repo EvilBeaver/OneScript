9192f067760e2f89a5d6a992fb0cc6190f181b24
ret-Phoenix <phoenixusa@yandex.ru>
2017-05-29 00:55:13 +0300

    Наброски работы с дисками (проект не собирается, коммит для переноса на другую машину)

---
+++ src/ScriptEngine.HostedScript/Library/DriveInfo/DriveInfo.cs
@@ -0,0 +1,137 @@
+﻿using ScriptEngine.HostedScript.Library;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace onescript_extensions.DriveInfo
+{
+    [ContextClass("ИнформацияОДиске", "DriveInfo")]
+    public class DriveInfo : AutoContext<DriveInfo>
+    {
+        private System.IO.DriveInfo _driveInfo;
+
+        public DriveInfo(string driveName)
+        {
+            DriveInfo = new System.IO.DriveInfo(driveName);
+        }
+
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor(IValue driveName)
+        {
+            return new DriveInfo(driveName.AsString());
+        }
+
+
+        /// <summary>
+        /// Указывает объем доступного свободного места на диске в байтах.
+        /// </summary>
+        [ContextProperty("Доступно")]
+        public Int64 AvailableFreeSpace
+        {
+            get { return DriveInfo.AvailableFreeSpace; }
+        }
+
+        /// <summary>
+        /// Получает имя файловой системы
+        /// </summary>
+        [ContextProperty("ИмяФС")]
+        public string DriveFormat
+        {
+            get { return DriveInfo.DriveFormat; }
+        }
+
+        [ContextProperty("ТипДиска", "DriveType")]
+        public DriveType DriveTypeProp
+        {
+            get { return ValueFactory.Create( DriveInfo.DriveType ); }
+        }
+
+        /// <summary>
+        /// Получает значение, указывающее состояние готовности диска.
+        /// </summary>
+        [ContextProperty("Готов")]
+        public bool IsReady
+        {
+            get
+            {
+                return DriveInfo.IsReady;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает имя диска
+        /// </summary>
+        [ContextProperty("Имя")]
+        public string Name
+        {
+            get
+            {
+                return DriveInfo.Name;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает корневой каталог диска.
+        /// </summary>
+        [ContextProperty("КорневойКаталог")]
+        public IValue RootDirectory
+        {
+            get
+            {
+                return new FileContext(DriveInfo.RootDirectory.FullName);
+            }
+        }
+
+        /// <summary>
+        /// Возвращает общий объем свободного места, доступного на диске, в байтах
+        /// </summary>
+        [ContextProperty("ОбщийОбъемСвободногоМеста")]
+        public Int64 TotalFreeSpace
+        {
+            get { return DriveInfo.TotalFreeSpace; }
+        }
+
+        /// <summary>
+        /// Возвращает общий размер места для хранения на диске в байтах.
+        /// </summary>
+        [ContextProperty("РазмерДиска")]
+        public Int64 TotalSize
+        {
+            get { return DriveInfo.TotalSize; }
+        }
+
+        /// <summary>
+        /// Возвращает или задает метку тома диска.
+        /// </summary>
+        [ContextProperty("МеткаТома")]
+        public string VolumeLabel
+        {
+            get
+            {
+                return DriveInfo.VolumeLabel;
+            }
+            set
+            {
+                DriveInfo.VolumeLabel = value;
+            }
+        }
+
+        public System.IO.DriveInfo DriveInfo
+        {
+            get
+            {
+                return _driveInfo;
+            }
+
+            set
+            {
+                _driveInfo = value;
+            }
+        }
+    }
+}
---
+++ src/ScriptEngine.HostedScript/Library/DriveInfo/DriveTypeEnum.cs
@@ -0,0 +1,90 @@
+﻿using ScriptEngine.Machine.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptEngine.Machine;
+
+namespace onescript_extensions.DriveInfo
+{
+    [SystemEnum("ТипДиска", "DriveType")]
+    class DriveTypeEnum : EnumerationContext
+    {
+
+        const System.IO.DriveType CDRom = System.IO.DriveType.CDRom;
+        //const int SHA1 = "SHA1";
+        //const int SHA256 = "SHA256";
+        //const int SHA384 = "SHA384";
+        //const int SHA512 = "SHA512";
+        //const int CRC32 = "CRC32";
+
+        //public DriveTypeEnum(TypeDescriptor typeRepresentation, TypeDescriptor valuesType) : base(typeRepresentation, valuesType)
+        //{
+        //}
+
+        /// <summary>
+        /// Диск является устройством оптических дисков, такие как компакт-ДИСК или DVD-диск.
+        /// </summary>
+        [ContextProperty("ОптическийДиск")]
+        public int CDRom
+        {
+            get { return (int)System.IO.DriveType.CDRom; }
+        }
+
+        /// <summary>
+        /// Диск является жестким диском.
+        /// </summary>
+        [ContextProperty("ЖесткийДиск")]
+        public int Fixed
+        {
+            get { return (int)System.IO.DriveType.Fixed; }
+        }
+
+        /// <summary>
+        /// Диск является сетевым диском.
+        /// </summary>
+        [ContextProperty("СетевойДиск")]
+        public int Network
+        {
+            get { return (int)System.IO.DriveType.Network; }
+        }
+
+        /// <summary>
+        /// Диск не имеет корневой каталог.
+        /// </summary>
+        [ContextProperty("НеИмеетКорневойКаталог")]
+        public int NoRootDirectory
+        {
+            get { return (int)System.IO.DriveType.NoRootDirectory; }
+        }
+
+        /// <summary>
+        /// Диск является диском ОЗУ.
+        /// </summary>
+        [ContextProperty("ДискОЗУ")]
+        public int Ram
+        {
+            get { return (int)System.IO.DriveType.Ram; }
+        }
+
+        /// <summary>
+        /// Диск является съемное запоминающее устройство, например, дисковод гибких дисков или USB-устройство флэш-памяти.
+        /// </summary>
+        [ContextProperty("СъемноеЗапоминающееУстройство")]
+        public int Removable
+        {
+            get { return (int)System.IO.DriveType.Removable; }
+        }
+
+        /// <summary>
+        /// Тип диска неизвестен.
+        /// </summary>
+        [ContextProperty("Неизвестный")]
+        public int Unknown
+        {
+            get { return (int)System.IO.DriveType.Unknown; }
+        }
+
+    }
+}
