8eb0c7650bba7d815f4a8d95aeb8ba8e8085acc4
EvilBeaver <ovsiankin.aa@gmail.com>
2017-06-20 13:51:06 +0300

    Ключи -check -compile выдают ошибку: Object reference not set to an instance of an object. fixes #457

---
+++ src/oscript/DoNothingHost.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ScriptEngine.HostedScript;
+using ScriptEngine.HostedScript.Library;
+
+namespace oscript
+{
+    class DoNothingHost : IHostApplication
+    {
+        public void Echo(string str, MessageStatusEnum status = MessageStatusEnum.Ordinary)
+        {
+            
+        }
+
+        public void ShowExceptionInfo(Exception exc)
+        {
+            
+        }
+
+        public bool InputString(out string result, int maxLen)
+        {
+            result = "";
+            return true;
+        }
+
+        public string[] GetCommandLineArguments()
+        {
+            return new[]
+            {
+                ""
+            };
+        }
+    }
+}
