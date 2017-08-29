57131e3c1b01c10cc4fd81c8e4df8c9fd8a1b529
Faithfinder <faithfinder@yandex.ru>
2017-08-29 12:18:13 +0300

    Первый набросок тестов движка

---
+++ src/oscriptTest/oscriptTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace oscript
+{
+    [TestFixture]
+    public class oscriptTests
+    {
+        [Test, Category("NoArgs")]
+        public void ShowUsageBehaviourExecutes()
+        {
+            var args = new string[0];
+            Program.Main(args);
+        }
+
+        [Test, Category("Make")]
+        public void BasicMakeBehaviourWorks()
+        {
+            var tempFolder = Path.GetTempPath();
+            const string exeName = "test.exe";
+
+            var osFullPath = Path.GetFullPath("test.os");
+            var exeFullPath = Path.Combine(tempFolder, exeName);
+
+            if (File.Exists(exeFullPath))
+            {
+                File.Delete(exeFullPath);
+            }
+
+            var args = new[]
+            {
+                "-make",
+                osFullPath,
+                exeFullPath
+            };
+
+            Program.Main(args);
+
+            var exeExists = File.Exists(exeFullPath);
+            Assert.IsTrue(exeExists);
+        }
+    }
+}
