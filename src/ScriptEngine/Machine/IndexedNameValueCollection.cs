c89bc21d79512c09a0b074344b4a7bb338d2dec3
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2017-09-27 17:42:22 +0300

    Видимость класса

--- src/ScriptEngine/Machine/IndexedNameValueCollection.cs
+++ src/ScriptEngine/Machine/IndexedNameValueCollection.cs
@@ -18,7 +18,7 @@ namespace ScriptEngine.Machine
     /// Уже создано много с похожим функционалом по всему проекту. Постепенно можно переводить сюда.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    class IndexedNameValueCollection<T> : IEnumerable<T>
+    public class IndexedNameValueCollection<T> : IEnumerable<T>
     {
         private readonly Dictionary<string, int> _nameIndex = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
         private readonly List<T> _values;
