2e0613ea62fb3d17987223d21ea4194a20c234f7
@jdeshin <j.deshin@hotmail.com>
2018-01-10 07:04:21 -0800

    Добавил везде: if (appSettings["handlerLoadingPolicy"] == "strict")                             throw; // Must fail!

--- src/ASPNETHandler/ASPNETHandler.cs
+++ src/ASPNETHandler/ASPNETHandler.cs
@@ -19,6 +19,8 @@ using ScriptEngine.Machine;
 using ScriptEngine.Environment;
 using ScriptEngine.HostedScript;
 
+using System.Runtime.CompilerServices;
+
 namespace OneScript.ASPNETHandler
 {
     public class ASPNETHandler : IHttpHandler, System.Web.SessionState.IRequiresSessionState
@@ -42,7 +44,10 @@ namespace OneScript.ASPNETHandler
 
             System.Collections.Specialized.NameValueCollection appSettings = System.Web.Configuration.WebConfigurationManager.AppSettings;
 
+            TextWriter logWriter = OpenLog(appSettings);
+
             _cachingEnabled = (appSettings["cachingEnabled"] == "true");
+            WriteToLog(logWriter, "Start assemblies loading.");
 
             foreach (string assemblyName in appSettings.AllKeys)
             {
@@ -53,11 +58,17 @@ namespace OneScript.ASPNETHandler
                         _assembliesForAttaching.Add(System.Reflection.Assembly.Load(assemblyName));
                     }
                     // TODO: Исправить - должно падать. Если конфиг сайта неработоспособен - сайт не должен быть работоспособен.
-                    // Спорное утверждение. Это загрузка внешних компонент aka sql etc. 
-                    // И что из-за одной dll, которой нет физически, она не используется и она есть в конфиге огрести неработоспособное приложение? 
-                    catch {/*не загрузилась, ничего не делаем*/ }
+                    catch(Exception ex)
+                    {
+                        WriteToLog(logWriter, "Error loading assembly: " + assemblyName + " " + ex.Message);
+                        if (appSettings["handlerLoadingPolicy"] == "strict")
+                            throw; // Must fail!
+                    }
                 }
             }
+
+            WriteToLog(logWriter, "Stop assemblies loading.");
+            CloseLog(logWriter);
         }
 
         public ASPNETHandler()
@@ -65,21 +76,7 @@ namespace OneScript.ASPNETHandler
             System.Collections.Specialized.NameValueCollection appSettings = System.Web.Configuration.WebConfigurationManager.AppSettings;
 
             // Инициализируем логгирование, если надо
-            string logPath = appSettings["logToPath"];
-            StreamWriter logWriter = null;
-
-            try
-            {
-                if (logPath != null)
-                {
-                    logPath = HttpContext.Current.Server.MapPath(logPath);
-                    string logFileName = Guid.NewGuid().ToString().Replace("-", "") + ".txt";
-                    logWriter = File.CreateText(Path.Combine(logPath, logFileName));
-                }
-            }
-            catch { /*что-то не так, ничего не делаем. Возможно нет папки или прав на запись.*/}
-
-            // Если определена секция logToPath и удалось создать/открыть файл, считаем, что логгирование включено
+            TextWriter logWriter = OpenLog(appSettings);
 
             WriteToLog(logWriter, "Start loading.");
 
@@ -100,12 +97,12 @@ namespace OneScript.ASPNETHandler
                     {
                         // Возникла проблема при аттаче сборки
                         WriteToLog(logWriter, "Assembly attaching error: " + ex.Message);
+                        if (appSettings["handlerLoadingPolicy"] == "strict")
+                            throw;
                     }
                 }
 
                 //Загружаем библиотечные скрипты aka общие модули
-
-
                 string libPath = appSettings["commonModulesPath"];
 
                 if (libPath != null)
@@ -137,13 +134,13 @@ namespace OneScript.ASPNETHandler
                         {
                             // Возникла проблема при загрузке файла os, логгируем, если логгирование включено
                             WriteToLog(logWriter, "Error loading " + System.IO.Path.GetFileNameWithoutExtension(filePathName) + " : " + ex.Message);
+                            if (appSettings["handlerLoadingPolicy"] == "strict")
+                                throw;
                         }
                     }
                 }
 
                 // метод настраивает внутренние переменные у SystemGlobalContext
-                // давай использовать эту опцию для разных вот таких недоделанных случаев
-                // потом будем или удалять или менять название опции, если это опционально
                 if (appSettings["advancedFeatures"] == "true")
                     _hostedScript.SetGlobalEnvironment(new ASPNetApplicationHost(), new AspEntryScriptSrc());
 
@@ -154,27 +151,43 @@ namespace OneScript.ASPNETHandler
                 // Возникла проблема при инициализации хэндлера
                 WriteToLog(logWriter, ex.Message);
 
-                if (appSettings["errorActionPreferenceWhenHandlerCreating"] == "stop")
+                if (appSettings["handlerLoadingPolicy"] == "strict")
                     throw; // Must fail!
             }
             finally
             {
                 WriteToLog(logWriter, "End loading.");
-                // Закрываем лог, если надо
-                if (logWriter != null)
-                {
-                    try
-                    {
-                        logWriter.Flush();
-                        logWriter.Close();
-                    }
-                    catch
-                    { /*что-то не так, ничего не делаем.*/ }
-                }
+                CloseLog(logWriter);
             }
         }
 
-        void WriteToLog(TextWriter logWriter, string message)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static TextWriter OpenLog(System.Collections.Specialized.NameValueCollection appSettings = null)
+        {
+            if (appSettings == null)
+                appSettings = System.Web.Configuration.WebConfigurationManager.AppSettings;
+
+            string logPath = appSettings["logToPath"];
+
+            try
+            {
+                if (logPath != null)
+                {
+                    logPath = HttpContext.Current.Server.MapPath(logPath);
+                    string logFileName = Guid.NewGuid().ToString().Replace("-", "") + ".txt";
+                    return File.CreateText(Path.Combine(logPath, logFileName));
+                }
+                else
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void WriteToLog(TextWriter logWriter, string message)
         {
             if (logWriter == null)
                 return;
@@ -185,43 +198,26 @@ namespace OneScript.ASPNETHandler
             catch { /* что-то не так, ничего не делаем */ }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void CloseLog(TextWriter logWriter)
+        {
+            if (logWriter != null)
+            {
+                try
+                {
+                    logWriter.Flush();
+                    logWriter.Close();
+                }
+                catch
+                { /*что-то не так, ничего не делаем.*/ }
+            }
+        }
+
         public void ProcessRequest(HttpContext context)
         {
             try
             {
-                #region Загружаем скрипт (файл .os)
-                // Кэшируем исходный файл, если файл изменился (изменили скрипт .os) загружаем заново
-                // В Linux под Mono не работает подписка на изменение файла.
-                LoadedModuleHandle? module = null;
-                ObjectCache cache = MemoryCache.Default;
-
-                if (_cachingEnabled)
-                {
-                    module = cache[context.Request.PhysicalPath] as LoadedModuleHandle?;
-
-                    if (module == null)
-                    {
-                        CacheItemPolicy policy = new CacheItemPolicy();
-
-                        List<string> filePaths = new List<string>();
-                        filePaths.Add(context.Request.PhysicalPath);
-                        policy.ChangeMonitors.Add(new HostFileChangeMonitor(filePaths));
-
-                        // Загружаем файл и помещаем его в кэш
-                        module = LoadByteCode(context.Request.PhysicalPath);
-                        cache.Set(context.Request.PhysicalPath, module, policy);
-                    }
-                }
-                else
-                {
-                    module = LoadByteCode(context.Request.PhysicalPath);
-                }
-
-                #endregion
-
-                var runner = CreateServiceInstance(module.Value);
-
-                ProduceResponse(context, runner);
+                CallScriptHandler(context);
             }
             finally
             {
@@ -229,6 +225,7 @@ namespace OneScript.ASPNETHandler
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void ProduceResponse(HttpContext context, IRuntimeContextInstance runner)
         {
             int methodIndex = runner.FindMethod("ОбработкаВызоваHTTPСервиса");
@@ -263,12 +260,14 @@ namespace OneScript.ASPNETHandler
             context.Response.Charset = response.ContentCharset;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private IRuntimeContextInstance CreateServiceInstance(LoadedModuleHandle module)
         {
             var runner = _hostedScript.EngineInstance.NewObject(module);
             return runner;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private LoadedModuleHandle LoadByteCode(string filePath)
         {
             var code = _hostedScript.EngineInstance.Loader.FromFile(filePath);
@@ -276,6 +275,44 @@ namespace OneScript.ASPNETHandler
             var byteCode = compiler.CreateModule(code);
             var module = _hostedScript.EngineInstance.LoadModuleImage(byteCode);
             return module;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void CallScriptHandler(HttpContext context)
+        {
+            #region Загружаем скрипт (файл .os)
+            // Кэшируем исходный файл, если файл изменился (изменили скрипт .os) загружаем заново
+            // В Linux под Mono не работает подписка на изменение файла.
+            LoadedModuleHandle? module = null;
+            ObjectCache cache = MemoryCache.Default;
+
+            if (_cachingEnabled)
+            {
+                module = cache[context.Request.PhysicalPath] as LoadedModuleHandle?;
+
+                if (module == null)
+                {
+                    CacheItemPolicy policy = new CacheItemPolicy();
+
+                    List<string> filePaths = new List<string>();
+                    filePaths.Add(context.Request.PhysicalPath);
+                    policy.ChangeMonitors.Add(new HostFileChangeMonitor(filePaths));
+
+                    // Загружаем файл и помещаем его в кэш
+                    module = LoadByteCode(context.Request.PhysicalPath);
+                    cache.Set(context.Request.PhysicalPath, module, policy);
+                }
+            }
+            else
+            {
+                module = LoadByteCode(context.Request.PhysicalPath);
+            }
+
+            #endregion
+
+            var runner = CreateServiceInstance(module.Value);
+
+            ProduceResponse(context, runner);
         }
     }
 }
