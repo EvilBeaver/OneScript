4945398ed20d3c82ce1e26cc954717c324b1bcc1
Dmitriy Korolev <d.korolev@live.ru>
2018-02-16 00:46:21 +0700

    Добавил лицензию

--- src/ScriptEngine.HostedScript/Library/ByteOrderMarkUsageEnum.cs
+++ src/ScriptEngine.HostedScript/Library/ByteOrderMarkUsageEnum.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
 
 namespace ScriptEngine.HostedScript.Library
 {
