b16d9d7465556750e4e094da7735a16168859dcd
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-03-07 12:06:30 +0300

    Объявлены устаревшими методы с хендлами модулей

--- src/ScriptEngine/Environment/ScriptModuleHandle.cs
+++ src/ScriptEngine/Environment/ScriptModuleHandle.cs
@@ -4,15 +4,19 @@ Mozilla Public License, v.2.0. If a copy of the MPL
 was not distributed with this file, You can obtain one 
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
+
+using System;
 using ScriptEngine.Machine;
 
 namespace ScriptEngine.Environment
 {
+    [Obsolete]
     public struct ScriptModuleHandle
     {
         internal ModuleImage Module { get; set; }
     }
 
+    [Obsolete]
     public struct LoadedModuleHandle
     {
         internal LoadedModule Module { get; set; }
