2202cd9b0263563212e62fd0ada8ec8e2e7da2e1
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2018-09-17 23:27:35 +0300

    Префикс лицензии

--- src/ScriptEngine/Machine/Reflection/WrappedMethodInfo.cs
+++ src/ScriptEngine/Machine/Reflection/WrappedMethodInfo.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 using ScriptEngine.Machine.Contexts;
 
