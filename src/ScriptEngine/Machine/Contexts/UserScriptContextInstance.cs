edc5fca882876a8a7837a65bb4417ea561787f95
Dmitriy Korolev <d.korolev@live.ru>
2018-11-15 09:51:38 +0700

    Рефакторинг кода конструктора

--- src/ScriptEngine/Machine/Contexts/UserScriptContextInstance.cs
+++ src/ScriptEngine/Machine/Contexts/UserScriptContextInstance.cs
@@ -55,32 +55,17 @@ namespace ScriptEngine.Machine.Contexts
 
             if (methId > -1)
             {
-                bool hasParamsError = false;
                 var procInfo = GetMethodInfo(methId);
 
                 int procParamsCount = procInfo.Params.Count();
 
-                if (procParamsCount < constructorParamsCount)
-                {
-                    hasParamsError = true;
-                }
+                int reqParamsCount = procInfo.Params.Count(x => !x.HasDefaultValue);
 
-                int reqParams = 0;
-                foreach (var itm in procInfo.Params)
-                {
-                    if (!itm.HasDefaultValue) reqParams++;
-                }
-                if (reqParams > constructorParamsCount)
-                {
-                    hasParamsError = true;
-                }
-                if (hasParamsError)
-                {
+                if (constructorParamsCount != reqParamsCount)
                     throw new RuntimeException("Параметры конструктора: "
-                        + "необходимых параметров: " + Math.Min(procParamsCount, reqParams).ToString()
+                        + "необходимых параметров: " + Math.Min(procParamsCount, reqParamsCount).ToString()
                         + ", передано параметров " + constructorParamsCount.ToString()
                         );
-                }
 
                 CallAsProcedure(methId, ConstructorParams);
             }
