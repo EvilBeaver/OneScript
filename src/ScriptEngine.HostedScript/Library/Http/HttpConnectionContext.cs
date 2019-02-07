d220a6d8c49d2d38080a9f2ebba29cd910ba6d43
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2019-02-08 01:05:13 +0300

    В HTTPСоединение выполнять автоматическую распаковку gzip

--- src/ScriptEngine.HostedScript/Library/Http/HttpConnectionContext.cs
+++ src/ScriptEngine.HostedScript/Library/Http/HttpConnectionContext.cs
@@ -60,7 +60,6 @@ namespace ScriptEngine.HostedScript.Library.Http
             Timeout = timeout;
             _proxy = proxy;
             UseOSAuthentication = useOSAuth;
-            AllowRedirect = true;
         }
 
         [ContextProperty("ИспользоватьАутентификациюОС", "UseOSAuthentication", CanWrite=false)]
@@ -206,6 +205,7 @@ namespace ScriptEngine.HostedScript.Library.Http
             var resourceUri = new Uri(uriBuilder.Uri, resource);
 
             var request = (HttpWebRequest)HttpWebRequest.Create(resourceUri);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
             if (User != "" || Password != "")
             {
                 request.Credentials = new NetworkCredential(User, Password);
