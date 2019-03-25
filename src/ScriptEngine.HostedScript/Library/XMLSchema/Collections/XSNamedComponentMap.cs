b7f473d25a49ea2bac81d9f52b153aa513584849
yukon39 <yukon39@gmail.com>
2019-03-25 09:47:27 +0200

    добавлена колллекция именованных компонетов

---
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Collections/XSNamedComponentMap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+
+namespace ScriptEngine.HostedScript.Library.XMLSchema
+{
+    [ContextClass("КоллекцияИменованныхКомпонентXS", "XSNamedComponentMap")]
+    public class XSNamedComponentMap : AutoContext<XSNamedComponentMap>, ICollectionContext, IEnumerable<IXSNamedComponent>
+    {
+        private readonly List<IXSNamedComponent> _items;
+
+        internal XSNamedComponentMap() => _items = new List<IXSNamedComponent>();
+
+        internal void Add(IXSNamedComponent value) => _items.Add(value);
+        internal void Delete(IXSNamedComponent value) => _items.Remove(value);
+
+        #region OneScript
+
+        #region Methods
+
+        [ContextMethod("Количество", "Count")]
+        public int Count() => _items.Count;
+
+        [ContextMethod("Получить", "Get")]
+        public IXSNamedComponent Get(int index) => _items[index];
+
+        public IXSNamedComponent Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IXSNamedComponent Get(string name, string namespaceURI)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #endregion
+
+        #region ICollectionContext
+
+        public CollectionEnumerator GetManagedIterator() => new CollectionEnumerator(GetEnumerator());
+
+        #endregion
+
+        #region IEnumerable
+
+        public IEnumerator<IXSNamedComponent> GetEnumerator() => _items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+
+    }
+}
---
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Interfaces/IXSNamedComponent.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ScriptEngine.HostedScript.Library.XMLSchema
+{
+    public interface IXSNamedComponent : IXSComponent
+    {
+        string Name { get; set; }
+    }
+}
+    //ОпределениеПростогоТипаXS, 
+    //ОпределениеСоставногоТипаXS, 
+    //ОпределениеГруппыАтрибутовXS, 
+    //ОпределениеГруппыМоделиXS, 
+    //ОбъявлениеАтрибутаXS, 
+    //ОбъявлениеЭлементаXS, 
+    //ОпределениеОграниченияИдентичностиXS, 
+    //ОпределенияXPathXS, 
+    //ОбъявлениеНотацииXS
