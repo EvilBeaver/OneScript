e9839a7e2c9f3b36e8081658387e20c590c185bd
yukon39 <yukon39@gmail.com>
2019-03-25 11:02:57 +0200

    добавлено НедопустимыеПодстановкиXS

---
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Collections/XSDisallowedSubstitutionsUnion.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Schema;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine.HostedScript.Library.XMLSchema
+{
+    [ContextClass("ОбъединениеНедопустимыхПодстановкиXS", "XSDisallowedSubstitutionsUnion")]
+    public class XSDisallowedSubstitutionsUnion: AutoContext<XMLSchema>
+    {
+        private ArrayImpl _values;
+
+        private bool Contains(XmlSchemaDerivationMethod _value)
+        {
+            XSDisallowedSubstitutions _enumValue = EnumerationXSDisallowedSubstitutions.FromNativeValue(_value);
+            IValue _idx = _values.Find(_enumValue);
+            return (_idx.DataType != DataType.Undefined);
+        }
+
+        internal XSDisallowedSubstitutionsUnion() => _values = ArrayImpl.Constructor();
+
+        #region OneScript
+
+        #region Properties
+
+        [ContextProperty("Все", "All")]
+        public bool All => Contains(XmlSchemaDerivationMethod.All);
+
+        [ContextProperty("Ограничение", "Restriction")]
+        public bool Restriction => Contains(XmlSchemaDerivationMethod.Restriction);
+
+        [ContextProperty("Расширение", "Extension")]
+        public bool Extension => Contains(XmlSchemaDerivationMethod.Extension);
+
+        #endregion
+
+        #region Methods
+       
+        [ContextMethod("Значения", "Values")]
+        public ArrayImpl Values() => _values;
+
+        #endregion
+
+        #endregion
+    }
+}
