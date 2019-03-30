76638f54a8bcded5ce67a72eb50499faff52c8ea
yukon39 <yukon39@gmail.com>
2019-03-30 02:38:12 +0200

    Добавлена реализация фасета длины

---
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Interfaces/IXSFacet.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ScriptEngine.HostedScript.Library.XMLSchema
+{
+    public interface IXSFacet : IXSAnnotated
+    {
+        XSSimpleTypeDefinition SimpleTypeDefinition { get; }
+        string LexicalValue { get; set; }
+        bool Fixed { get; set; }
+    }
+}
