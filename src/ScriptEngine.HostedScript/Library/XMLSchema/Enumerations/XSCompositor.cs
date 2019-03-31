833e92ff91a308ff4da03f66f7ac674f6a4d0d7f
yukon39 <yukon39@gmail.com>
2019-03-31 03:00:46 +0200

    Добавлена группа модели

---
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Enumerations/XSCompositor.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Schema;
+
+namespace ScriptEngine.HostedScript.Library.XMLSchema
+{
+    /// <summary>
+    /// Вид группы модели
+    /// </summary>
+    /// <see cref="XmlSchemaGroupBase"/>
+    [EnumerationType("XSCompositor", "ВидГруппыМоделиXS")]
+    public enum XSCompositor
+    {
+        /// <summary>
+        /// Требует наличия элементов группы без требования последовательности
+        /// </summary>
+        /// <see cref="XmlSchemaAll"/>
+        [EnumItem("All", "Все")]
+        All,
+
+        /// <summary>
+        /// Требует наличия только одного из элементов группы
+        /// </summary>
+        /// <see cref="XmlSchemaChoice"/>
+        [EnumItem("Choice", "Выбор")]
+        Choice,
+
+        /// <summary>
+        /// Требует чтобы элементы следовали в указанной последовательности
+        /// </summary>
+        /// <see cref="XmlSchemaSequence"/>
+        [EnumItem("Sequence", "Последовательность")]
+        Sequence
+    }
+}
