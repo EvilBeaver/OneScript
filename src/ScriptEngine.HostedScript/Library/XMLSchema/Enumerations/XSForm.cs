ad1ecd6c3282e7614263b61ed952b2e3420ea93a
yukon39 <yukon39@gmail.com>
2019-04-02 19:59:22 +0200

    fix: ТестДолжен_ПроверитьЧтоВсеФайлыИмеютПрефиксЛицензии

--- src/ScriptEngine.HostedScript/Library/XMLSchema/Enumerations/XSForm.cs
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Enumerations/XSForm.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Xml.Schema;
 using ScriptEngine.Machine;
--- src/ScriptEngine.HostedScript/Library/XMLSchema/Interfaces/IXSComponent.cs
+++ src/ScriptEngine.HostedScript/Library/XMLSchema/Interfaces/IXSComponent.cs
@@ -1,4 +1,10 @@
-﻿using ScriptEngine.Machine;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using ScriptEngine.Machine;
 using System.Xml.Schema;
 
 namespace ScriptEngine.HostedScript.Library.XMLSchema
