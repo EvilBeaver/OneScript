5d5311bd60b90443cb9d6f7a82e0daba5279ecac
yukon39 <yukon39@gmail.com>
2019-04-16 17:30:44 +0200

    Сериализатор теперь работает.

--- src/ScriptEngine.HostedScript/Library/XDTO/XDTOSerializer.cs
+++ src/ScriptEngine.HostedScript/Library/XDTO/XDTOSerializer.cs
@@ -5,6 +5,7 @@ was not distributed with this file, You can obtain one
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
 
+using System;
 using System.Xml;
 using System.Xml.Schema;
 using ScriptEngine.HostedScript.Library.Xml;
@@ -22,43 +23,46 @@ namespace ScriptEngine.HostedScript.Library.XDTO
 
         private XDTOSerializer() { }
 
-        private void WriteXMLSimpleData(XmlWriterImpl xmlWriter, string name, string value, XMLExpandedName type, XMLTypeAssignment typeAssigment, XMLForm form)
+        private void WriteXMLSimpleData(XmlWriterImpl xmlWriter,
+                                        string name,
+                                        IValue value,
+                                        XMLExpandedName type,
+                                        XMLTypeAssignment typeAssigment,
+                                        XMLForm form)
         {
             XmlNamespaceContext namespaceContext;
+            string xmlValue = XMLString(value);
             switch (form)
             {
                 case XMLForm.Attribute:
                     namespaceContext = xmlWriter.NamespaceContext;
-                    if (namespaceContext.LookupPrefix(XmlSchema.Namespace) == ValueFactory.Create())
-                        xmlWriter.WriteNamespaceMapping("", XmlSchema.Namespace);
+                    AddNamespaceMapping(namespaceContext, xmlWriter, "", XmlSchema.Namespace);
 
                     xmlWriter.WriteStartAttribute(name);
-                    xmlWriter.WriteText(value);
+                    xmlWriter.WriteText(xmlValue);
                     xmlWriter.WriteEndAttribute();
                     break;
 
                 case XMLForm.Text:
-                    xmlWriter.WriteText(value);
+                    xmlWriter.WriteText(xmlValue);
                     break;
 
                 default:
-                    namespaceContext = xmlWriter.NamespaceContext;
 
                     xmlWriter.WriteStartElement(name);
-                    if (namespaceContext.LookupPrefix(XmlSchema.Namespace) == ValueFactory.Create())
-                        xmlWriter.WriteNamespaceMapping("", XmlSchema.Namespace);
 
-                    if (namespaceContext.LookupPrefix(XmlSchema.InstanceNamespace) == ValueFactory.Create())
-                        xmlWriter.WriteNamespaceMapping("xsi", XmlSchema.InstanceNamespace);
+                    namespaceContext = xmlWriter.NamespaceContext;
+                    AddNamespaceMapping(namespaceContext, xmlWriter, "", XmlSchema.Namespace);
+                    AddNamespaceMapping(namespaceContext, xmlWriter, "xsi", XmlSchema.InstanceNamespace);
 
                     if (typeAssigment == XMLTypeAssignment.Explicit)
                     {
-                        xmlWriter.WriteStartAttribute("type", type.NamespaceURI);
+                        xmlWriter.WriteStartAttribute("type", XmlSchema.InstanceNamespace);
                         xmlWriter.WriteText(type.LocalName);
                         xmlWriter.WriteEndAttribute();
                     }
 
-                    xmlWriter.WriteText(value);
+                    xmlWriter.WriteText(xmlValue);
 
                     xmlWriter.WriteEndElement();
                     break;
@@ -70,18 +74,21 @@ namespace ScriptEngine.HostedScript.Library.XDTO
             if (form == XMLForm.Element)
             {
                 XmlNamespaceContext namespaceContext = xmlWriter.NamespaceContext;
-                if (namespaceContext.LookupPrefix(XmlSchema.Namespace) == ValueFactory.Create())
-                    xmlWriter.WriteNamespaceMapping("", XmlSchema.Namespace);
+                AddNamespaceMapping(namespaceContext, xmlWriter, "", XmlSchema.Namespace);
+                AddNamespaceMapping(namespaceContext, xmlWriter, "xsi", XmlSchema.InstanceNamespace);
 
-                if (namespaceContext.LookupPrefix(XmlSchema.InstanceNamespace) == ValueFactory.Create())
-                    xmlWriter.WriteNamespaceMapping("xsi", XmlSchema.InstanceNamespace);
-
-                xmlWriter.WriteStartElement("Undefined");
+                xmlWriter.WriteStartElement(name);
                 xmlWriter.WriteStartAttribute("nil", XmlSchema.InstanceNamespace);
                 xmlWriter.WriteText("true");
                 xmlWriter.WriteEndAttribute();
                 xmlWriter.WriteEndElement();
             }
+        }
+
+        private void AddNamespaceMapping(XmlNamespaceContext namespaceContext, XmlWriterImpl xmlWriter, string prefix, string uri)
+        {
+            if (namespaceContext.LookupPrefix(uri) == ValueFactory.Create())
+                xmlWriter.WriteNamespaceMapping(prefix, uri);
         }
 
         #region OneScript
@@ -108,26 +115,88 @@ namespace ScriptEngine.HostedScript.Library.XDTO
         //ЗаписатьXDTO(WriteXDTO)
 
         [ContextMethod("ЗаписатьXML", "WriteXML")]
-        public void WriteXML(XmlWriterImpl xmlWriter, IValue value, XMLTypeAssignment typeAssigment = XMLTypeAssignment.Explicit, XMLForm form = XMLForm.Element)
+        public void WriteXML(XmlWriterImpl xmlWriter,
+                             IValue value,
+                             XMLTypeAssignment typeAssigment = XMLTypeAssignment.Implicit,
+                             XMLForm form = XMLForm.Element)
+        {
+            switch (value.DataType)
+            {
+                case DataType.Undefined:
+
+                    WriteXML(xmlWriter, value, "Undefined", typeAssigment, form);
+                    break;
+
+                case DataType.String:
+
+                    WriteXML(xmlWriter, value, "string", typeAssigment, form);
+                    break;
+
+                case DataType.Number:
+
+                    WriteXML(xmlWriter, value, "decimal", typeAssigment, form);
+                    break;
+
+                case DataType.Boolean:
+
+                    WriteXML(xmlWriter, value, "boolean", typeAssigment, form);
+                    break;
+
+                case DataType.Date:
+
+                    WriteXML(xmlWriter, value, "dateTime", typeAssigment, form);
+                    break;
+
+                case DataType.Object:
+
+                    IRuntimeContextInstance valueObject = value.AsObject();
+                    if (valueObject is IXDTOSerializableXML seriazable)
+                        seriazable.WriteXML(xmlWriter);
+                    else
+                        throw RuntimeException.InvalidArgumentType();
+                    break;
+
+                default:
+                    throw RuntimeException.InvalidArgumentType();
+            }
+        }
+
+        public void WriteXML(XmlWriterImpl xmlWriter,
+                             IValue value,
+                             string name,
+                             XMLTypeAssignment typeAssigment = XMLTypeAssignment.Implicit,
+                             XMLForm form = XMLForm.Element)
         {
             XMLExpandedName xmlType;
             switch (value.DataType)
             {
                 case DataType.Undefined:
 
-                    WriteXMLUndefined(xmlWriter, "Undefined", form);
+                    WriteXMLUndefined(xmlWriter, name, form);
                     break;
 
                 case DataType.String:
 
                     xmlType = new XMLExpandedName(XmlSchema.InstanceNamespace, "string");
-                    WriteXMLSimpleData(xmlWriter, "string", value.AsString(), xmlType, typeAssigment, form);
+                    WriteXMLSimpleData(xmlWriter, name, value, xmlType, typeAssigment, form);
                     break;
 
                 case DataType.Number:
 
                     xmlType = new XMLExpandedName(XmlSchema.InstanceNamespace, "decimal");
-                    WriteXMLSimpleData(xmlWriter, "decimal", XMLString(value), xmlType, typeAssigment, form);
+                    WriteXMLSimpleData(xmlWriter, name, value, xmlType, typeAssigment, form);
+                    break;
+
+                case DataType.Boolean:
+
+                    xmlType = new XMLExpandedName(XmlSchema.InstanceNamespace, "boolean");
+                    WriteXMLSimpleData(xmlWriter, name, value, xmlType, typeAssigment, form);
+                    break;
+
+                case DataType.Date:
+
+                    xmlType = new XMLExpandedName(XmlSchema.InstanceNamespace, "dateTime");
+                    WriteXMLSimpleData(xmlWriter, name, value, xmlType, typeAssigment, form);
                     break;
 
                 case DataType.Object:
@@ -150,25 +219,74 @@ namespace ScriptEngine.HostedScript.Library.XDTO
         //ПрочитатьXDTO(ReadXDTO)
 
         [ContextMethod("ПрочитатьXML", "ReadXML")]
-        public IValue ReadXML(XmlReaderImpl xmlReader, IValue valueType)
+        public IValue ReadXML(XmlReaderImpl xmlReader, IValue valueType = null)
         {
-            TypeDescriptor typeValue = TypeManager.GetTypeDescriptorFor(valueType.GetRawValue());
+            TypeTypeValue typeValue = null;
 
-            if (typeValue.Equals(TypeDescriptor.FromDataType(DataType.String)))
+            if (valueType is TypeTypeValue typeTypeValue)
+                typeValue = typeTypeValue;
+
+            else if (xmlReader.NodeType == xmlNodeEnum.FromNativeValue(XmlNodeType.Element))
+            {
+                IValue xsiType = xmlReader.GetAttribute(ValueFactory.Create("type"), XmlSchema.InstanceNamespace);
+                IValue xsiNil = xmlReader.GetAttribute(ValueFactory.Create("nil"), XmlSchema.InstanceNamespace);
+
+                if (xsiType.DataType == DataType.String)
+                {
+                    switch (xsiType.AsString())
+                    {
+                        case "string":
+                            typeValue = new TypeTypeValue("String");
+                            break;
+
+                        case "decimal":
+                            typeValue = new TypeTypeValue("Number");
+                            break;
+
+                        case "boolean":
+                            typeValue = new TypeTypeValue("Boolean");
+                            break;
+
+                        case "dateTime":
+                            typeValue = new TypeTypeValue("Date");
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else if(xsiNil.DataType == DataType.String)
+                    typeValue = new TypeTypeValue("Undefined");
+            };
+
+            if (typeValue == null)
+                throw RuntimeException.InvalidArgumentValue();
+
+            Type implType = TypeManager.GetImplementingClass(typeValue.Value.ID);
+
+            IValue result = ValueFactory.Create();
+
+            if (typeValue.Equals(new TypeTypeValue("Undefined")))
+            {
+                result = ValueFactory.Create();
+                xmlReader.Skip();
+            }
+            else if (implType == typeof(DataType))
             {
                 xmlReader.Read();
                 if (xmlReader.NodeType == xmlNodeEnum.FromNativeValue(XmlNodeType.Text))
-                    return ValueFactory.Create(xmlReader.Value);
+                {
+                    result = XMLValue(typeValue, xmlReader.Value);
+                    xmlReader.Read();
+                }
                 else
                     throw RuntimeException.InvalidArgumentValue();
             }
-            //else if (typeValue.Equals(TypeDescriptor.FromDataType(DataType.Object)))
-            //{
-            //    return new XMLSchema.XMLSchema(xmlReader);
-            //}
-            //else
-            //    throw RuntimeException.InvalidArgumentType();
-            return ValueFactory.Create();
+            else if (typeof(IXDTOSerializableXML).IsAssignableFrom(implType))
+                result = Activator.CreateInstance(implType, xmlReader) as IValue;
+
+            xmlReader.Read();
+            return result;
         }
 
         #endregion
