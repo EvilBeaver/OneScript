a2f3ef9b1eb61921da031f32103e54b4b1ea9989
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2019-04-24 19:06:35 +0300

    Реализованы методы встроенного макета

--- src/StandaloneRunner/InternalTemplate.cs
+++ src/StandaloneRunner/InternalTemplate.cs
@@ -5,6 +5,9 @@ was not distributed with this file, You can obtain one
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
 
+using System.IO;
+using System.Net;
+
 using ScriptEngine.HostedScript;
 using ScriptEngine.HostedScript.Library.Binary;
 
@@ -12,24 +15,47 @@ namespace StandaloneRunner
 {
     public class InternalTemplate : ITemplate
     {
+        private string _tempFileName;
+
+        private BinaryDataContext _data;
+        
         public InternalTemplate(byte[] data, TemplateKind kind)
         {
             Kind = kind;
+            _data = new BinaryDataContext(data);
         }
 
         public void Dispose()
         {
-            //throw new System.NotImplementedException();
+            if (_tempFileName != null)
+            {
+                try
+                {
+                    File.Delete(_tempFileName);
+                }
+                catch
+                {// now errors on exit
+                }
+            }
         }
 
         public string GetFilename()
         {
-            throw new System.NotImplementedException();
+            if (_tempFileName == null)
+            {
+                _tempFileName = Path.GetTempFileName();
+                using (var fs = new FileStream(_tempFileName, FileMode.OpenOrCreate))
+                {
+                    fs.Write(_data.Buffer,0,_data.Buffer.Length);
+                }
+            }
+
+            return _tempFileName;
         }
 
         public BinaryDataContext GetBinaryData()
         {
-            throw new System.NotImplementedException();
+            return _data;
         }
 
         public TemplateKind Kind { get; }
