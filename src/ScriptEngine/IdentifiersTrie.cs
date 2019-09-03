c44d1e5a24402d65710e0bf127c1eb20d97ed6c9
Andrey Ovsiankin <ovsiankin.aa@gmail.com>
2019-09-03 19:34:13 +0300

    Вынесено префиксное дерево в отдельный файл

---
+++ src/ScriptEngine/IdentifiersTrie.cs
@@ -0,0 +1,72 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+using System;
+
+namespace ScriptEngine
+{
+    public class IdentifiersTrie
+    {
+        private class TrieNode
+        {
+            public char charL;
+            public char charU;
+            public int value;
+            public TrieNode sibl;
+            public TrieNode next;
+
+            public TrieNode Find(char ch)
+            {
+                var node = sibl;
+                while (node != null)
+                {
+                    if (node.charL == ch || node.charU == ch)
+                        return node;
+                    node = node.sibl;
+                }
+                return null;
+            }
+        }
+
+        TrieNode root;
+
+        public IdentifiersTrie() { root = new TrieNode(); }
+
+        public void Add(string str, int val)
+        {
+            TrieNode node = root;
+            foreach (char ch in str)
+            {
+                TrieNode key = node.Find(ch);
+                if (key == null)
+                {
+                    key = new TrieNode() { charL = Char.ToLower(ch), charU = Char.ToUpper(ch), value = -1 };
+                    key.sibl = node.sibl;
+                    node.sibl = key;
+                    key.next = new TrieNode();
+                }
+                node = key.next;
+            }
+
+            node.value = val;
+        }
+
+        public int Find(string str)
+        {
+            TrieNode node = root;
+            foreach (char ch in str)
+            {
+                TrieNode key = node.Find(ch);
+                if (key == null)
+                    return -1;
+
+                node = key.next;
+            }
+
+            return node.value;
+        }
+    }
+}
--- src/ScriptEngine/Machine/Contexts/ContextMethodMapper.cs
+++ src/ScriptEngine/Machine/Contexts/ContextMethodMapper.cs
@@ -9,73 +9,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 namespace ScriptEngine.Machine.Contexts
 {
-    public class IdentifiersTrie
-    {
-        private class TrieNode
-        {
-            public char charL;
-            public char charU;
-            public int value;
-            public TrieNode sibl;
-            public TrieNode next;
-
-            public TrieNode Find(char ch)
-            {
-                var node = sibl;
-                while (node != null)
-                {
-                    if (node.charL == ch || node.charU == ch)
-                        return node;
-                    node = node.sibl;
-                }
-                return null;
-            }
-        }
-
-        TrieNode root;
-
-        public IdentifiersTrie() { root = new TrieNode(); }
-
-        public void Add(string str, int val)
-        {
-            TrieNode node = root;
-            foreach (char ch in str)
-            {
-                TrieNode key = node.Find(ch);
-                if (key == null)
-                {
-                    key = new TrieNode() { charL = Char.ToLower(ch), charU = Char.ToUpper(ch), value = -1 };
-                    key.sibl = node.sibl;
-                    node.sibl = key;
-                    key.next = new TrieNode();
-                }
-                node = key.next;
-            }
-
-            node.value = val;
-        }
-
-        public int Find(string str)
-        {
-            TrieNode node = root;
-            foreach (char ch in str)
-            {
-                TrieNode key = node.Find(ch);
-                if (key == null)
-                    return -1;
-
-                node = key.next;
-            }
-
-            return node.value;
-        }
-    }
-
-
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ContextMethodAttribute : Attribute
     {
