7ecdaa480f444683cd146bc8cd1b1734af521023
Dmitriy Korolev <d.korolev@live.ru>
2019-09-04 00:00:08 +0700

    Рефакторинг глобального контекста

--- src/ScriptEngine.HostedScript/Library/Timezones/TimeZoneConverter.cs
+++ src/ScriptEngine.HostedScript/Library/Timezones/TimeZoneConverter.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ScriptEngine.HostedScript
+namespace ScriptEngine.HostedScript.Library.Timezones
 {
     public class TimeZoneConverter
     {
