dbf62bdb315e5ea6bd88c130fa08cc2a3216aeeb
Andrey Ovsiankin <ovsiankin.aa@gmail.com>
2019-10-15 19:03:19 +0300

    Исправление #938 в первом приближении

--- src/ScriptEngine/Environment/ModuleInformation.cs
+++ src/ScriptEngine/Environment/ModuleInformation.cs
@@ -1,4 +1,4 @@
-﻿/*----------------------------------------------------------
+/*----------------------------------------------------------
 This Source Code Form is subject to the terms of the 
 Mozilla Public License, v.2.0. If a copy of the MPL 
 was not distributed with this file, You can obtain one 
@@ -13,10 +13,20 @@ using OneScript.Language.LexicalAnalysis;
 
 namespace ScriptEngine.Environment
 {
+    [Serializable]
     public class ModuleInformation
     {
+        [NonSerialized]
+        private ISourceCodeIndexer _codeIndexer;
+
         public string ModuleName { get; set; }
-        internal ISourceCodeIndexer CodeIndexer { get; set; }
+
+        public ISourceCodeIndexer CodeIndexer
+        {
+            get => _codeIndexer;
+            set => _codeIndexer = value;
+        }
+
         public string Origin { get; set; }
 
         public override string ToString()
