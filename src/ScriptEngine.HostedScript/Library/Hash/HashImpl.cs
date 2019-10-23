f320b7658bddb86465e877e39e8af4a46eb2bc40
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2019-10-23 04:14:34 +0300

    Revert "Научить ХешированиеДанных работать с потоками. Улучшить совместимость с 1С"
    This reverts commit 2563733a

--- src/ScriptEngine.HostedScript/Library/Hash/HashImpl.cs
+++ src/ScriptEngine.HostedScript/Library/Hash/HashImpl.cs
@@ -4,32 +4,56 @@ Mozilla Public License, v.2.0. If a copy of the MPL
 was not distributed with this file, You can obtain one 
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
-
 using System;
-using System.IO;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using ScriptEngine.HostedScript.Library.Binary;
 using ScriptEngine.Machine;
 using ScriptEngine.Machine.Contexts;
+using System.Security.Cryptography;
+using System.IO;
+using ScriptEngine.HostedScript.Library;
+using ScriptEngine.HostedScript.Library.Binary;
 
 namespace ScriptEngine.HostedScript.Library.Hash
 {
     [ContextClass("ХешированиеДанных", "DataHashing")]
-    public class HashImpl : AutoContext<HashImpl>
+    public class HashImpl : AutoContext<HashImpl>, IDisposable
     {
-        private readonly HashAlgorithm _provider;
-        private readonly IValue _hashFunction;
-        private byte[] _hash;
+        protected HashAlgorithm _provider;
+        protected IValue _enumValue;
+        protected CombinedStream _toCalculate=new CombinedStream();
+        protected bool _calculated;
+        protected byte[] _hash;
 
-        public HashImpl(HashAlgorithm provider, IValue hashFunction)
+        public HashImpl(HashAlgorithm provider, IValue enumValue)
         {
             _provider = provider;
-            _hashFunction = hashFunction;
+            _enumValue = enumValue;
+            _calculated = false;
+        }
+
+        public byte[] InternalHash
+        {
+            get
+            {
+                if (!_calculated)
+                {
+                    _hash = _provider.ComputeHash(_toCalculate);
+                    _calculated = true;
+                }
+                return _hash;
+            }
         }
 
         [ContextProperty("ХешФункция", "HashFunction")]
-        public IValue HashFunction => _hashFunction;
+        public IValue Extension
+        {
+            get
+            {
+                return _enumValue;
+            }
+        }
 
         [ContextProperty("ХешСумма", "HashSum")]
         public IValue Hash
@@ -39,63 +63,51 @@ namespace ScriptEngine.HostedScript.Library.Hash
                 if (_provider is Crc32)
                 {
                     var buffer = new byte[4];
-                    Array.Copy(_hash, buffer, 4);
+                    Array.Copy(InternalHash, buffer, 4);
                     if (BitConverter.IsLittleEndian)
                         Array.Reverse(buffer);
                     var ret = BitConverter.ToUInt32(buffer, 0);
-                    return ValueFactory.Create(ret);
+                    return ValueFactory.Create((decimal)ret);
                 }
-
-                return new BinaryDataContext(_hash);
+                return new BinaryDataContext(InternalHash);
             }
         }
 
         [ContextProperty("ХешСуммаСтрокой", "HashSumOfString")]
-        public string HashString => BitConverter.ToString(_hash).Replace("-", "");
+        public string HashString
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < InternalHash.Length; i++)
+                    sb.Append(InternalHash[i].ToString("X2"));
+                return sb.ToString();
+            }
+        }
 
 
         [ContextMethod("Добавить", "Append")]
-        public void Append(IValue data, uint count = 0)
+        public void Append(IValue toAdd)
         {
-            switch (data.DataType)
+            byte[] buffer = null;
+            if (toAdd.DataType==DataType.String)
             {
-                case DataType.String:
-                    _hash = _provider.ComputeHash(Encoding.UTF8.GetBytes(data.AsString()));
-                    break;
-                case DataType.Object:
-                    switch (data)
-                    {
-                        case GenericStream stream:
-                        {
-                            var underlyingStream = stream.GetUnderlyingStream();
-                            if (count == 0) // Читать все до конца потока
-                                _hash = _provider.ComputeHash(underlyingStream);
-                            else
-                            {
-                                const int bufferSize = 4096;
-                                var buffer = new byte[bufferSize];
-                                var remain = Convert.ToInt32(count);
-                                var readBytes = 0;
-                                do
-                                {
-                                    readBytes = underlyingStream.Read(buffer, 0, Math.Min(bufferSize, remain));
-                                    if (readBytes > 0)
-                                        _hash = _provider.ComputeHash(buffer, 0, readBytes);
-                                    remain -= readBytes;
-                                } while (readBytes > 0);
-                            }
-
-                            break;
-                        }
-                        case BinaryDataContext binaryData:
-                            _hash = _provider.ComputeHash(binaryData.Buffer);
-                            break;
-                    }
-
-                    break;
-                default:
-                    throw RuntimeException.InvalidArgumentType();
+                buffer = Encoding.UTF8.GetBytes(toAdd.AsString());
             }
+            else if(toAdd.DataType==DataType.Object)
+            {
+                try
+                {
+                    var binaryData = toAdd as BinaryDataContext;
+                    buffer = binaryData.Buffer;
+                }
+                catch
+                {
+                     throw RuntimeException.InvalidArgumentType();
+                }
+            }
+            AddStream(new MemoryStream(buffer));
+            
         }
 
         [ContextMethod("ДобавитьФайл", "AppendFile")]
@@ -103,17 +115,38 @@ namespace ScriptEngine.HostedScript.Library.Hash
         {
             if (!File.Exists(path))
                 throw RuntimeException.InvalidArgumentType();
-            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                _hash = _provider.ComputeHash(stream);
-            }
+            AddStream(new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
         }
 
-        [ScriptConstructor(Name = "По указанной хеш-функции")]
-        public static HashImpl Constructor(IValue hashFunction)
+        [ContextMethod("Очистить", "Clear")]
+        public void Clear()
         {
-            var objectProvider = HashFunctionEnum.GetProvider(hashFunction);
-            return new HashImpl(objectProvider, hashFunction);
+            _toCalculate.Close();
+            _toCalculate.Dispose();
+            _toCalculate = new CombinedStream();
+            _calculated = false;
+        }
+
+
+        [ScriptConstructor(Name = "По указанной хеш-функции")]
+        public static HashImpl Constructor(IValue providerEnum)
+        {
+            var objectProvider = HashFunctionEnum.GetProvider(providerEnum);
+            return new HashImpl(objectProvider, providerEnum);
+        }
+
+        public void Dispose()
+        {
+            _toCalculate.Close();
+            _toCalculate.Dispose();
+        }
+
+        private void AddStream(Stream stream)
+        {
+            _toCalculate.AddStream(stream);
+            _toCalculate.Seek(0, SeekOrigin.Begin);
+            _calculated = false;
+            
         }
     }
 }
