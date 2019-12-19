a33282c8d10bc26aedf9e509a858b31d9f16404c
boris stoyanov <info@stoyanov-boris.ru>
2019-12-19 12:44:22 +0500

    Начало работы с ldap
    Добавил класс DirectoryEntry

---
+++ src/ScriptEngine.HostedScript/Library/LDAP/DirectoryEntry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using AD = System.DirectoryServices;
+
+namespace ScriptEngine.HostedScript.Library.LDAP
+{
+    [ContextClass("ЗаписьКаталога", "DirectoryEntry")]
+    class DirectoryEntryImpl : AutoContext<DirectoryEntryImpl>
+    {
+        private AD.DirectoryEntry _directoryEntry;
+
+        [ContextProperty("Имя", "Name")]
+        public string Name => _directoryEntry.Name;
+
+        #region Constructors
+        public DirectoryEntryImpl()
+        {
+            _directoryEntry = new AD.DirectoryEntry();
+        }
+
+        public DirectoryEntryImpl(string path)
+        {
+            _directoryEntry = new AD.DirectoryEntry(path);
+        }
+
+        public DirectoryEntryImpl(string path, string username, string password)
+        {
+            _directoryEntry = new AD.DirectoryEntry(path, username, password);
+        }
+
+        /// <summary>
+        /// Конструктор создания записи каталога по заданному пути.
+        /// </summary>
+        /// <param name="name">Путь к объекту в дереве каталога.</param>
+        [ScriptConstructor(Name = "По пути объекта")]
+        public static DirectoryEntryImpl Constructor(IValue name)
+        {
+            var direntry = new DirectoryEntryImpl(name.AsString());
+            return direntry;
+        }
+
+        #endregion
+        
+        /// <summary>
+        /// Проверяет существование записи каталога по заданному пути.
+        /// </summary>
+        /// <param name="path">Путь к объекту в дереве каталога.</param>
+        [ContextMethod("Существует", "Exists")]
+        public static bool Exists(IValue path)
+        {
+            return AD.DirectoryEntry.Exists(path.AsString());
+        }
+    }
+}
