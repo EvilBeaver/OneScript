8c8eaa5a6ea66ac81b27ff3f34569c82b33b5494
boris stoyanov <info@stoyanov-boris.ru>
2019-12-30 18:13:28 +0500

    Болванка поиска

---
+++ src/ScriptEngine.HostedScript/Library/LDAP/DirectorySearcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.DirectoryServices;
+
+namespace ScriptEngine.HostedScript.Library.LDAP
+{
+    [ContextClass("ПоискВКаталоге", "DirectorySearcher")]
+    class DirectorySearcherImpl : AutoContext<DirectorySearcherImpl>
+    {
+    }
+}
