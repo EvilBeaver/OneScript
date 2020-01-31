e2eaaf3d6e36abce0d6b200469270e04acbd8640
EvilBeaver <ovsiankin.aa@gmail.com>
2020-01-31 12:26:27 +0300

    Причесал немного генерацию текста

--- src/ScriptEngine/Machine/RuntimeExceptions.cs
+++ src/ScriptEngine/Machine/RuntimeExceptions.cs
@@ -76,12 +76,12 @@ namespace ScriptEngine.Machine
         
         public static RuntimeException MethodNotFoundException(string methodName)
         {
-            return new RuntimeException(string.Format("Метод объекта не обнаружен ({0})", methodName));
+            return new RuntimeException($"Метод объекта не обнаружен ({methodName})");
         }
 
         public static RuntimeException MethodNotFoundException(string methodName, string objectName)
         {
-            return new RuntimeException(string.Format("Метод объекта не обнаружен ({{{1}}}::{0})", methodName, objectName));
+            return new RuntimeException($"Метод объекта не обнаружен ({{{objectName}}}::{methodName})");
         }
 
         public static RuntimeException ValueIsNotObjectException()
