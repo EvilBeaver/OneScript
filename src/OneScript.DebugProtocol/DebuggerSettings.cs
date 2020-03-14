971152bce97aa37edc9051cce9fe4c4796981f7e
Dmitriy Korolev <d.korolev@live.ru>
2020-03-14 11:25:44 +0700

    Фикс вывода длинных представлений переменных

---
+++ src/OneScript.DebugProtocol/DebuggerSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OneScript.DebugProtocol
+{
+    public static class DebuggerSettings
+    {
+        public const int MAX_BUFFER_SIZE = 5000000;
+        public const int MAX_PRESENTATION_LENGTH = (int)(MAX_BUFFER_SIZE / 2.5);
+    }
+}
