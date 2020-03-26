21f09e6b13d6e5bfc1255e1996b95959bd93db16
EvilBeaver <ovsiankin.aa@gmail.com>
2020-03-26 14:20:54 +0300

    Рефакторинг метода разбора параметров командной строки

--- src/oscript/DebugBehavior.cs
+++ src/oscript/DebugBehavior.cs
@@ -33,6 +33,31 @@ namespace oscript
 
             return executor.Execute();
         }
-        
+
+        public static AppBehavior Create(CmdLineHelper helper)
+        {
+            var arg = helper.Next();
+            int port = 2801;
+            if (arg != null && arg.StartsWith("-port="))
+            {
+                var prefixLen = ("-port=").Length;
+                if (arg.Length > prefixLen)
+                {
+                    var value = arg.Substring(prefixLen);
+                    if (!Int32.TryParse(value, out port))
+                    {
+                        Output.WriteLine("Incorrect port: " + value);
+                        return null;
+                    }
+                }
+            }
+            else if(arg != null)
+            {
+                var path = arg;
+                return new DebugBehavior(port, path, helper.Tail());
+            }
+
+            return null;
+        }
     }
 }
