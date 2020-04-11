1082ce3911ac9656c5504a7bfe0a24057f33e044
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2020-04-12 00:00:16 +0300

    Добавил директив препроцессора для WCF

--- src/OneScript.DebugProtocol/Binder.cs
+++ src/OneScript.DebugProtocol/Binder.cs
@@ -4,6 +4,8 @@ Mozilla Public License, v.2.0. If a copy of the MPL
 was not distributed with this file, You can obtain one 
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
+
+#if NETFRAMEWORK
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,3 +33,4 @@ namespace OneScript.DebugProtocol
         }
     }
 }
+#endif
--- src/OneScript.DebugProtocol/IDebugEventListener.cs
+++ src/OneScript.DebugProtocol/IDebugEventListener.cs
@@ -4,6 +4,9 @@ Mozilla Public License, v.2.0. If a copy of the MPL
 was not distributed with this file, You can obtain one
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
+#if NETFRAMEWORK
+using System.ServiceModel;
+#endif
 
 namespace OneScript.DebugProtocol
 {
