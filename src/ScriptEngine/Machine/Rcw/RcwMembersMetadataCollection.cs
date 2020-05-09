5dae8b0efedb475946bbe0c488680b7640b62751
Dmitriy Korolev <d.korolev@live.ru>
2020-05-09 10:07:36 +0700

    Добавил лицензии

--- src/ScriptEngine/Machine/Rcw/RcwMembersMetadataCollection.cs
+++ src/ScriptEngine/Machine/Rcw/RcwMembersMetadataCollection.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections;
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace ScriptEngine.Machine.Rcw
 {
