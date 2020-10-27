f2054dba6471410623953ed4a0a0bdf6efac55da
Kandrashin Denis <mail@lintest.ru>
2020-10-27 22:32:16 +0300

    Ужалены дубликаты

--- src/ScriptEngine.HostedScript/Library/NativeApi/NativeApiProxy.cs
+++ src/ScriptEngine.HostedScript/Library/NativeApi/NativeApiProxy.cs
@@ -7,9 +7,7 @@ namespace ScriptEngine.HostedScript.Library.NativeApi
     {
         private const String ProxyDll = "ScriptEngine.NativeApi.dll";
 
-        public delegate void StringDelegate(IntPtr ptr);
-
-        public delegate void VariantDelegate(IntPtr ptr);
+        public delegate void PointerDelegate(IntPtr ptr);
 
         public static String Str(IntPtr p)
         {
@@ -36,10 +34,10 @@ namespace ScriptEngine.HostedScript.Library.NativeApi
         public static extern bool IsPropWritable(IntPtr ptr, long lPropNum);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern void GetPropName(IntPtr ptr, long lPropNum, long lPropAlias, StringDelegate response);
+        public static extern void GetPropName(IntPtr ptr, long lPropNum, long lPropAlias, PointerDelegate response);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern void GetPropVal(IntPtr ptr, long lPropNum, VariantDelegate response);
+        public static extern void GetPropVal(IntPtr ptr, long lPropNum, PointerDelegate response);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern void SetPropVal(IntPtr ptr, long lPropNum, ref NativeApiVariant value);
@@ -51,7 +49,7 @@ namespace ScriptEngine.HostedScript.Library.NativeApi
         public static extern long FindMethod(IntPtr ptr, string wsMethodName);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern void GetMethodName(IntPtr ptr, long lMethodNum, long lMethodAlias, StringDelegate response);
+        public static extern void GetMethodName(IntPtr ptr, long lMethodNum, long lMethodAlias, PointerDelegate response);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern long GetNParams(IntPtr ptr, long lMethodNum);
@@ -63,6 +61,6 @@ namespace ScriptEngine.HostedScript.Library.NativeApi
         public static extern bool CallAsProc(IntPtr ptr, long lMethodNum, ref NativeApiVariant paParams, long lSizeArray);
 
         [DllImport(ProxyDll, SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern bool CallAsFunc(IntPtr ptr, long lMethodNum, ref NativeApiVariant paParams, long lSizeArray, VariantDelegate response);
+        public static extern bool CallAsFunc(IntPtr ptr, long lMethodNum, ref NativeApiVariant paParams, long lSizeArray, PointerDelegate response);
     }
 }
