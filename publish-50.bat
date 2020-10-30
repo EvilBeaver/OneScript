565d010e57bd4ea837ebab42d6376af8bddd389f
Al Iczin <al@iczin.guru>
2020-10-31 01:09:51 +0300

    Работает архивация артефакатов

--- publish-50.bat
+++ publish-50.bat
@@ -1,10 +1,8 @@
 @echo off
 
-dotnet clean src\1Script.sln
-
 echo {  "sdk": {    "version": "5.0.100-rc.2.20479.15"  } } > global.json
 
 dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -f net5.0 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net50\win-x64\bin
 dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -f net5.0 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net50\win-x86\bin
-dotnet publish -p:PublishTrimmed=true -f net5.0 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net50\linux-X64\bin
+dotnet publish -p:PublishTrimmed=true -f net5.0 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net50\linux-x64\bin
 dotnet publish -p:PublishTrimmed=true -f net5.0 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net50\osx-x64\bin
--- publish.bat
+++ publish.bat
@@ -9,21 +9,21 @@ rmdir distrs /s /q
 echo {  "sdk": {    "version": "3.1.403"  } } > global.json
 
 dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x64\bin
-rem dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x86\bin
-rem dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\linux-X64\bin
-rem dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
+dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x86\bin
+dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\linux-X64\bin
+dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
 
-cd distrs\net31\win-x64\bin
+rem cd distrs\net31\win-x64\bin
 
-set OSLIB_LOADER_TRACE=1
+rem set OSLIB_LOADER_TRACE=1
 
-call opm install asserts
-call opm install tempfiles
-call opm install delegate
-call opm install fs
-call opm install 1testrunner
-call opm install 1bdd
+rem call opm install asserts
+rem call opm install tempfiles
+rem call opm install delegate
+rem call opm install fs
+rem call opm install 1testrunner
+rem call opm install 1bdd
 
-call 1testrunner -runall ..\..\..\..\tests
+rem call 1testrunner -runall ..\..\..\..\tests
 
-cd ..\..\..\..\
+rem cd ..\..\..\..\
