2ad95e25be76d936e39025ae96528290ff16c47b
Al Iczin <al@iczin.guru>
2020-10-30 16:25:42 +0300

    Не удалять дитрибутивы для 5-ки запуск тестов - пока падают

--- publish.bat
+++ publish.bat
@@ -1,5 +1,9 @@
 @echo off
 
+setlocal
+
+echo "Local FULL publish for each platform, with testing script-engine (not xunit) by 1testrunner and archive artifacts"
+
 dotnet clean src\1Script.sln
 
 rmdir distrs /s /q
@@ -12,3 +16,16 @@ dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r linu
 dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
 
 rm global.json
+
+cd distrs\net31\win-x64\bin
+
+call opm install asserts
+call opm install tempfiles
+call opm install delegate
+call opm install fs
+call opm install 1testrunner
+call opm install 1bdd
+
+call 1testrunner -runall ..\..\..\..\tests
+
+cd ..\..\..\..\
