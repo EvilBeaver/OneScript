603baabbe61cf7dce04b354714622a2226a90699
Al Iczin <al@iczin.guru>
2020-11-04 13:57:45 +0300

    Локально проверяемый полнофункциональный скрипт публикации - выдает 4 архива с движком проверяет тесты

--- publish.bat
+++ publish.bat
@@ -9,21 +9,21 @@ rmdir distrs /s /q
 echo {  "sdk": {    "version": "3.1.403"  } } > global.json
 
 dotnet publish -p:PublishTrimmed=false -p:PublishReadyToRun=false -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x64\bin
-rem dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x86\bin
-rem dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\linux-x64\bin
-rem dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
+dotnet publish -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunShowWarnings=true -f netcoreapp3.1 -p:DebugType=None -r win-x86  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\win-x86\bin
+dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r linux-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\linux-x64\bin
+dotnet publish -p:PublishTrimmed=true -f netcoreapp3.1 -p:DebugType=None -r osx-x64  -c Release --self-contained true --force -p:IncludeNativeLibrariesInSingleFile=true -p:CopyOutputSymbolsToPublishDirectory=false .\src\oscript\oscript.csproj -o distrs\net31\osx-x64\bin
 
 cd distrs\net31\win-x64\bin
 
 set OSLIB_LOADER_TRACE=1
 
 call opm install asserts
-rem call opm install tempfiles
-rem call opm install delegate
-rem call opm install fs
-rem call opm install 1testrunner
-rem call opm install 1bdd
+call opm install tempfiles
+call opm install delegate
+call opm install fs
+call opm install 1testrunner
+call opm install 1bdd
 
-rem call 1testrunner -runall ..\..\..\..\tests
+call 1testrunner -runall ..\..\..\..\tests
 
-rem cd ..\..\..\..\
+cd ..\..\..\..\
