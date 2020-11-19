9f015350f89035a9ffae17043a552b235f5bb601
lintest <mail@lintest.ru>
2020-11-19 11:42:22 +0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,30 +1,24 @@
-cd src
-cd ScriptEngine.NativeApi
+SET ROOT=%CD%
 
-cmake -E remove_directory bin
-cmake -E remove_directory build32
-cmake -E remove_directory build64
-
-mkdir bin\
-mkdir bin\Release\
-mkdir bin\Release\net452\
-mkdir bin\x86\Release\
-mkdir bin\x86\Release\net452\
+cmake -E remove_directory built\tmp\NativeApi
+mkdir built
+mkdir built\tmp
+mkdir built\tmp\NativeApi\
+cd built\tmp\NativeApi
 
 mkdir build64
 cd build64
-cmake .. -A x64
+cmake %ROOT%\src\ScriptEngine.NativeApi -A x64
 cmake --build . --config Release
 cd ..
 
 mkdir build32
 cd build32
-cmake .. -A Win32
+cmake %ROOT%\src\ScriptEngine.NativeApi -A Win32
 cmake --build . --config Release
 cd ..
 
-copy build64\Release\*.dll bin\Release\net452\
-copy build32\Release\*.dll bin\x86\Release\net452\
+copy build64\Release\*.dll %ROOT%\built\tmp\bin\
+copy build32\Release\*.dll %ROOT%\built\tmp\bin32\
 
-cd ..
-cd ..
+cd %ROOT%
