c5e0d8e80e80982447bd0bbccef72f4933b55907
Kandrashin Denis <mail@lintest.ru>
2021-01-12 22:28:19 +0300

    Сборка ScriptEngine.NativeApi.dll для Linux

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -2,6 +2,14 @@
 
 THISDIR=$(pwd)
 
+cd $THISDIR/../src/ScriptEngine.NativeApi
+mkdir build
+cd build
+cmake .. 
+cmake --build .
+cp ScriptEngine.NativeApi.dll $THISDIR
+cd $THISDIR
+
 cd $THISDIR/builders/deb
 docker build -t oscript/onescript-builder:deb .
 
