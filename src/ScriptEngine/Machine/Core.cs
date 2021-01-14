04a77eb721a41f048e72391eaec00794d2a11c42
Andrei Ovsiankin <ovsiankin.aa@gmail.com>
2021-01-14 16:10:20 +0300

    Флаги методов переведены на enum-флаги

--- src/ScriptEngine/Machine/Core.cs
+++ src/ScriptEngine/Machine/Core.cs
@@ -5,6 +5,7 @@ was not distributed with this file, You can obtain one
 at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
 using System;
+using System.Runtime.CompilerServices;
 
 namespace ScriptEngine.Machine
 {
@@ -184,21 +185,72 @@ namespace ScriptEngine.Machine
     {
         public string Name;
         public string Alias;
-        public bool IsFunction;
-        public bool IsExport;
-        [NonSerialized]
-        public bool IsDeprecated;
-        [NonSerialized]
-        public bool ThrowOnUseDeprecated;
         public ParameterDefinition[] Params;
         public AnnotationDefinition[] Annotations;
+        public MethodFlags Flags;
+
+        public bool IsFunction
+        {
+            get => IsSet(MethodFlags.IsFunction);
+            set => SetFlag(MethodFlags.IsFunction, value);
+        }
+        
+        public bool IsExport
+        {
+            get => IsSet(MethodFlags.IsExported);
+            set => SetFlag(MethodFlags.IsExported, value);
+        }
+        
+        public bool IsAsync
+        {
+            get => IsSet(MethodFlags.IsAsync);
+            set => SetFlag(MethodFlags.IsAsync, value);
+        }
+        
+        public bool IsDeprecated
+         {
+            get => IsSet(MethodFlags.IsDeprecated);
+            set => SetFlag(MethodFlags.IsDeprecated, value);
+        }
+        
+        public bool ThrowOnUseDeprecated
+        {
+            get => IsSet(MethodFlags.ThrowOnUsage);
+            set => SetFlag(MethodFlags.ThrowOnUsage, value);
+        }
 
         public int ArgCount => Params?.Length ?? 0;
-
         public int AnnotationsCount => Annotations?.Length ?? 0;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void SetFlag(MethodFlags flag) => Flags |= flag;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void SetFlag(MethodFlags flag, bool value)
+        { 
+            if(value) SetFlag(flag);
+            else ClearFlag(flag);
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void ClearFlag(MethodFlags flag) => Flags &= ~flag;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsSet(MethodFlags flag) => (Flags & flag) != 0;
     }
 
+    [Serializable]
+    [Flags]
+    public enum MethodFlags
+    {
+        Default = 0,
+        IsFunction = 1,
+        IsExported = 2,
+        IsDeprecated = 4,
+        ThrowOnUsage = 8,
+        IsAsync = 16
+    }
+    
     [Serializable]
     public struct ParameterDefinition
     {
