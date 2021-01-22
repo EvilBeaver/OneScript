0d59e7f4d888fed74df40ef9e483c466c8db192c
Al Iczin <al@iczin.guru>
2021-01-22 09:12:23 +0300

    а вот так совсем правильней

--- src/ScriptEngine.NativeApi/build-in-docker.bat
+++ src/ScriptEngine.NativeApi/build-in-docker.bat
@@ -1,16 +1,7 @@
-docker build -t oscript/onescript-builder:gcc  ^
+docker build --no-cache -t oscript/onescript-builder:gcc  ^
     ..\..\install\builders\nativeapi
 
 set SRC_PATH=%CD%
-set BUILD32_OUT=%SRC_PATH%\build32
-set BUILD64_OUT=%SRC_PATH%\build64
     
-docker run --rm -v %SRC_PATH%:/build/src -v %BUILD32_OUT%:/build/out  ^
-    -it oscript/onescript-builder:gcc cmake ^
-    -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=ON  ^
-    --build ../src
-
-docker run --rm -v %SRC_PATH%:/build/src -v %BUILD64_OUT%:/build/out  ^
-    -it oscript/onescript-builder:gcc cmake ^
-    -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=ON  ^
-    --build ../src
+docker run --rm -v %SRC_PATH%:/build/src ^
+    -it oscript/onescript-builder:gcc sh ./build.sh
