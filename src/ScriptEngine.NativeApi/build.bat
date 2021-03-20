25e73cad248ebaf8f179e844768a64f47eafac1a
Kandrashin Denis <mail@lintest.ru>
2021-03-20 17:43:43 +0300

    Переделана сборка при помощи CMake

--- src/ScriptEngine.NativeApi/build.bat
+++ src/ScriptEngine.NativeApi/build.bat
@@ -1,11 +1,11 @@
 cmake -E make_directory build32w
 cd build32w
 cmake -A Win32 --build ..
-cmake --config Release --build .
+cmake --build . --config Release
 cd ..
 
 cmake -E make_directory build64w
 cd build64w
 cmake -A x64 --build ..
-cmake --config Release --build .
+cmake --build . --config Release
 cd ..
