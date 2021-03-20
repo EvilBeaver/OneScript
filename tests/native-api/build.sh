f9acdeea33fde93a33c7c9aa7ea37f0d427e7270
Kandrashin Denis <mail@lintest.ru>
2021-03-20 16:48:00 +0300

    Сборка примера внешней компоненты

---
+++ tests/native-api/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cmake -E make_directory build32
+cd build32
+cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=ON --build ..
+cmake --build .
+cd ..
+
+cmake -E make_directory build64
+cd build64
+cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=OFF --build ..
+cmake --build .
+cd ..
