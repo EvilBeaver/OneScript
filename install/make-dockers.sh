6b44e74646b9c9062a7cd0a862e99ab093e9f29b
Kandrashin Denis <mail@lintest.ru>
2021-07-09 00:33:01 +0300

    docker run

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -9,4 +9,5 @@ cd $THISDIR/builders/rpm
 docker build -t oscript/onescript-builder:rpm .
 
 cd $THISDIR/../src/ScriptEngine.NativeApi
-docker build -t oscript/onescript-builder:gcc -v .:/build/src -f $THISDIR/builders/nativeapi/Dockerfile .
+docker build -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile .
+docker run --rm -v .:/build/src -it oscript/onescript-builder:gcc sh ./build.sh
