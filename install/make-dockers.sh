65977c52d25499bee6be633b480a939d8ee2a77f
Kandrashin Denis <mail@lintest.ru>
2021-07-09 21:56:05 +0300

    gcc

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -10,5 +10,5 @@ docker build -t oscript/onescript-builder:rpm .
 
 cd $THISDIR/../src/ScriptEngine.NativeApi
 docker build -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile .
-docker cp oscript/onescript-builder:gcc:/build/build32/ScriptEngine.NativeApi32.so .
-docker cp oscript/onescript-builder:gcc:/build/build64/ScriptEngine.NativeApi64.so .
+docker cp gcc:/build/build32/ScriptEngine.NativeApi32.so .
+docker cp gcc:/build/build64/ScriptEngine.NativeApi64.so .
