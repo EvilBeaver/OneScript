e3910839d550f58d68ca0c25d9a960536b4dd7ae
lintest <mail@lintest.ru>
2021-07-09 08:18:46 +0300

    Delete build-in-docker.bat

--- src/ScriptEngine.NativeApi/build-in-docker.bat
+++
@@ -1,7 +0,0 @@
-docker build --no-cache -t oscript/onescript-builder:gcc  ^
-    ..\..\install\builders\nativeapi
-
-set SRC_PATH=%CD%
-    
-docker run --rm -v %SRC_PATH%:/build/src ^
-    -it oscript/onescript-builder:gcc sh ./build.sh
