845d50033ca32b285dabcc22af60c5b4ca21e60c
Kandrashin Denis <mail@lintest.ru>
2021-07-10 10:06:42 +0300

    chmod +x build.sh

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -9,5 +9,6 @@ cd $THISDIR/builders/rpm
 docker build -t oscript/onescript-builder:rpm .
 
 cd $THISDIR/../src/ScriptEngine.NativeApi
+chmod +x build.sh
 docker build --no-cache -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile .
 docker run -v $(pwd):/src oscript/onescript-builder:gcc
