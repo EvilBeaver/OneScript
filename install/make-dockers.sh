cf0ba0ee353f24cbc412e90bfa39f25e983c173f
Kandrashin Denis <mail@lintest.ru>
2021-07-10 13:10:36 +0300

    THISDIR=$(pwd)

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 THISDIR=$(pwd)
+docker build --no-cache -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile ..
 
 cd $THISDIR/builders/deb
 docker build -t oscript/onescript-builder:deb .
 
 cd $THISDIR/builders/rpm
 docker build -t oscript/onescript-builder:rpm .
-
-cd $THISDIR
-docker build --no-cache -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile ..
