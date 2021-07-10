9b1d8e1a82c0e0e1a53d7383e30343cd3c245bba
Kandrashin Denis <mail@lintest.ru>
2021-07-10 11:05:50 +0300

    Delete file

--- install/x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-THISDIR=$(pwd)
-
-cd $THISDIR/../src/ScriptEngine.NativeApi
-docker build -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile .
-docker run --rm -i -v $(pwd):/build/src oscript/onescript-builder:gcc sh ./build.sh
