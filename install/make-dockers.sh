4a03eacddb67ee455eeea3d66a3482104b8e88d3
Kandrashin Denis <mail@lintest.ru>
2021-07-10 10:21:27 +0300

    --no-cache

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -9,7 +9,7 @@ cd $THISDIR/builders/rpm
 docker build -t oscript/onescript-builder:rpm .
 
 cd $THISDIR/builders/gcc
-docker build -t oscript/onescript-builder:gcc .
+docker build --no-cache -t oscript/onescript-builder:gcc .
 
 cd $THISDIR/../src/ScriptEngine.NativeApi
 docker run -v $(pwd):/src oscript/onescript-builder:gcc
