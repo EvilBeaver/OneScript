295b2fc55987cc4b43bf18e512fde5287d26edee
Kandrashin Denis <mail@lintest.ru>
2021-07-13 00:13:54 +0300

    Временно добавлен ключ: --no-cache

--- install/make-dockers.sh
+++ install/make-dockers.sh
@@ -3,10 +3,10 @@
 THISDIR=$(pwd)
 
 cd $THISDIR/builders/deb
-docker build -t oscript/onescript-builder:deb .
+docker build -t --no-cache oscript/onescript-builder:deb .
 
 cd $THISDIR/builders/rpm
-docker build -t oscript/onescript-builder:rpm .
+docker build -t --no-cache oscript/onescript-builder:rpm .
 
 cd $THISDIR
-docker build -t oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile ..
+docker build -t --no-cache oscript/onescript-builder:gcc -f $THISDIR/builders/nativeapi/Dockerfile ..
