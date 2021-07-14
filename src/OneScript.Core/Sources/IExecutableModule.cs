04d91188bd4bea1624b8cf26084207aead03e511
Andrey Ovsiankin <ovsiankin.a.a@gmail.com>
2021-07-14 18:17:49 +0300

    Вынос данных об исходниках из Engine

---
+++ src/OneScript.Core/Sources/IExecutableModule.cs
@@ -0,0 +1,27 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using OneScript.Contexts;
+using OneScript.Sources;
+
+namespace OneScript.Runtime
+{
+    public interface IExecutableModule
+    {
+        IList<BslAnnotationAttribute> ModuleAttributes { get; }
+
+        IList<BslFieldInfo> Fields { get; }
+        
+        IList<BslPropertyInfo> Properties { get; }
+        
+        IList<BslMethodInfo> Methods { get; }
+        
+        ICodeSource Source { get; }
+    }
+}
---
+++ src/OneScript.Core/Sources/SourceCode.cs
@@ -0,0 +1,32 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+
+namespace OneScript.Sources
+{
+    public class SourceCode
+    {
+        private readonly ICodeSource _textSource;
+
+        public SourceCode(ICodeSource textSource)
+        {
+            _textSource = textSource;
+        }
+
+        public string ModuleName => _textSource.ModuleName;
+
+        public string Location => _textSource.Location;
+
+        public string GetSourceCode() => _textSource.GetSourceCode();
+        
+        public string GetCodeLine(int line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
