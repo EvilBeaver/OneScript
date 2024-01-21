71e2a0cb96c38c9e20086146bcc15900d5940457
EvilBeaver <ovsiankin.aa@gmail.com>
2024-01-21 23:51:24 +0300

    fixes #1389
    (cherry picked from commit 6d9357aa1aec004432685402eb9416a2e37829d0)

--- src/ScriptEngine/Machine/Contexts/UserScriptContextInstance.cs
+++ src/ScriptEngine/Machine/Contexts/UserScriptContextInstance.cs
@@ -1,4 +1,4 @@
-﻿/*----------------------------------------------------------
+/*----------------------------------------------------------
 This Source Code Form is subject to the terms of the 
 Mozilla Public License, v.2.0. If a copy of the MPL 
 was not distributed with this file, You can obtain one 
@@ -87,7 +87,7 @@ namespace ScriptEngine.Machine.Contexts
                 _asStringOverride = base.AsString;
             else
             {
-                var signature = GetMethodInfo(methId);
+                var signature = GetMethodInfo(GetOwnMethodCount()+methId);
                 if (signature.ArgCount != 2)
                     throw new RuntimeException("Обработчик получения представления должен иметь 2 параметра");
 
