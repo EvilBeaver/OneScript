785ea8fcbab80fe13c1f2abaf591ad7a5c439e3a
EvilBeaver <ovsiankin.aa@gmail.com>
2024-06-02 10:13:41 +0300

    Загрузчик зависимостей

---
+++ src/ScriptEngine/Libraries/DependencyLoader.cs
@@ -0,0 +1,34 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using OneScript.Sources;
+using ScriptEngine.Machine.Contexts;
+
+namespace ScriptEngine
+{
+    public class DependencyLoader
+    {
+        private readonly ScriptingEngine _engine;
+        private readonly RuntimeEnvironment _env;
+
+        public DependencyLoader(ScriptingEngine engine)
+        {
+            _engine = engine;
+            _env = engine.Environment;
+        }
+
+        public void AddClass(ICodeSource source, string className)
+        {
+            
+        }
+
+        public void AddModule(ICodeSource source, string moduleName)
+        {
+            
+        }
+    }
+}
