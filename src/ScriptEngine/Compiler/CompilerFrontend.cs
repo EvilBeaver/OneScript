39c265b13af26dd43eb13e88e24625211e09a6d9
Andrey Ovsyankin <ovsa@1c.ru>
2024-09-17 16:43:18 +0300

    Удалены неиспользуемые классы

--- src/ScriptEngine/Compiler/CompilerFrontend.cs
+++ src/ScriptEngine/Compiler/CompilerFrontend.cs
@@ -6,8 +6,6 @@ at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using OneScript.Commons;
 using OneScript.Compilation;
 using OneScript.Compilation.Binding;
 using OneScript.Contexts;
@@ -16,7 +14,6 @@ using OneScript.Execution;
 using OneScript.Language;
 using OneScript.Language.SyntaxAnalysis;
 using OneScript.Language.SyntaxAnalysis.AstNodes;
-using ScriptEngine.Machine;
 using ScriptEngine.Machine.Contexts;
 
 namespace ScriptEngine.Compiler
