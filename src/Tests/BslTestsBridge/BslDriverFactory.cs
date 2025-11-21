92ee7bbf62cb90cd57a35ba322bfc4291c8a28b9
EvilBeaver <ovsiankin.aa@gmail.com>
2025-11-21 14:44:23 +0300

    Работающее расширение, которое ничего не делает

---
+++ src/Tests/BslTestsBridge/BslDriverFactory.cs
@@ -0,0 +1,34 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System.Reflection;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine.ClientProtocol;
+using NUnit.Engine.Extensibility;
+
+namespace BslTestsBridge;
+
+[Extension]
+public class BslDriverFactory : IDriverFactory
+{
+    StreamWriter file = new StreamWriter("Extension.log", true);
+    
+    public bool IsSupportedTestFramework(AssemblyName reference)
+    {
+        file.WriteLine($"IsSupportedTestFramework: {reference.FullName}");
+        file.Flush();
+        
+        return reference.Name == "nunit.framework";
+    }
+
+    public IFrameworkDriver GetDriver(AssemblyName reference)
+    {
+        file.WriteLine($"GetDriver: {reference.FullName}");
+        file.Flush();
+        
+        return new BslFrameworkDriver();
+    }
+}
---
+++ src/Tests/BslTestsBridge/BslFrameworkDriver.cs
@@ -0,0 +1,41 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+
+namespace BslTestsBridge;
+
+public class BslFrameworkDriver : IFrameworkDriver
+{
+    public string Load(string testAssemblyPath, IDictionary<string, object> settings)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CountTestCases(string filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Run(ITestEventListener listener, string filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Explore(string filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StopRun(bool force)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ID { get; set; }
+}
