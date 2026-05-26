63ae1a11820bb313739e15f33049b1a0ae68ed75
EvilBeaver <ovsiankin.aa@gmail.com>
2026-05-26 11:33:26 +0300

    fixed #1678 Свойство СистемнаяИнформация.ПолнаяКоманднаяСтрока

--- src/OneScript.StandardLibrary/SystemEnvironmentContext.cs
+++ src/OneScript.StandardLibrary/SystemEnvironmentContext.cs
@@ -41,6 +41,12 @@ namespace OneScript.StandardLibrary
         /// </summary>
         [ContextProperty("ВерсияОС", "OSVersion")]
         public string OSVersion => Environment.OSVersion.VersionString;
+        
+        /// <summary>
+        /// Полная командная строка выполняемого приложения, включая исполняемый файл и все аргументы командной строки.
+        /// </summary>
+        [ContextProperty("ПолнаяКоманднаяСтрока", "FullCommandLine")]
+        public string FullCommandLine => Environment.CommandLine;
 
         /// <summary>
         /// Имя ядра ОС/
