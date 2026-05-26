a73172850733f61b3def3fb40bd2d22c7e3d7fb1
EvilBeaver <ovsiankin.aa@gmail.com>
2026-05-26 11:25:32 +0300

    Документировал IHostApplication

--- src/ScriptEngine.HostedScript/IHostApplication.cs
+++ src/ScriptEngine.HostedScript/IHostApplication.cs
@@ -9,11 +9,36 @@ using OneScript.StandardLibrary;
 
 namespace ScriptEngine.HostedScript
 {
+    /// <summary>
+    /// Интерфейс взаимодействия с приложением, которое хостит движок скрипта.
+    /// Хост-приложение может быть не консольным, а 3D-игрой или GUI-окном, поэтому движок не знает
+    /// куда выводить текст в каждом конкретном случае. Это определяет реализация IHostApplication.
+    /// </summary>
     public interface IHostApplication
     {
-		void Echo(string str, MessageStatusEnum status = MessageStatusEnum.Ordinary);
-        void ShowExceptionInfo(Exception exc);
+		/// <summary>
+		/// Вывод пользовательских сообщений методом Сообщить.
+		/// </summary>
+		/// <param name="str">Строка</param>
+		/// <param name="status">Статус сообщения (второй параметр метода Сообщить)</param>
+        void Echo(string str, MessageStatusEnum status = MessageStatusEnum.Ordinary);
+		
+        /// <summary>
+        /// Показать информацию о неперехваченном исключении.
+        /// </summary>
+        /// <param name="exc">Неперехваченное исключение</param>
+		void ShowExceptionInfo(Exception exc);
+        
+        /// <summary>
+        /// Ввод данных методом ВвестиСтроку. Хост должен предоставить средства ввода текста.
+        /// </summary>
         bool InputString(out string result, string prompt, int maxLen, bool multiline);
+        
+        /// <summary>
+        /// Аргументы командной строки, доступные скрипту в коллекции АргументыКоманднойСтроки.
+        /// Хост может при запуске получать собственные аргументы, которые не нужны или не должны быть видны в скриптах,
+        /// поэтому хост может отфильтровать строку запуска и предоставить скрипту только аргументы непосредственно скрипта.
+        /// </summary>
         string[] GetCommandLineArguments();
     }
 }
