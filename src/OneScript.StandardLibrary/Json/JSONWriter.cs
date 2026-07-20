493f13c16644ca5c4e121645bc8722412e32095d
Mr-Rm <m.rybakin@gmail.com>
2026-07-20 12:48:08 +0400

    к #1532: ОткрытьПоток для ЗаписьJSON

--- src/OneScript.StandardLibrary/Json/JSONWriter.cs
+++ src/OneScript.StandardLibrary/Json/JSONWriter.cs
@@ -13,6 +13,7 @@ using Newtonsoft.Json;
 using OneScript.Commons;
 using OneScript.Contexts;
 using OneScript.Exceptions;
+using OneScript.StandardLibrary.Binary;
 using OneScript.StandardLibrary.Text;
 using ScriptEngine.Machine;
 using ScriptEngine.Machine.Contexts;
@@ -20,29 +21,27 @@ using ScriptEngine.Machine.Contexts;
 namespace OneScript.StandardLibrary.Json
 {
     /// <summary>
-    /// 
     /// Предназначен для организации последовательной записи объектов и текстов JSON.
     /// </summary>
     [ContextClass("ЗаписьJSON", "JSONWriter")]
-    public class JSONWriter : AutoContext<JSONWriter>
+    public class JSONWriter : AutoContext<JSONWriter>, IDisposable
     {
         private const int INDENT_SIZE = 0;
+        private const string DEFAULT_ENCODING = "UTF-8";
 
         private JSONWriterSettings _settings;
         private JsonTextWriter _writer; // Объект из библиотеки Newtonsoft для работы с форматом JSON 
 
         StringWriter _stringWriter;
+        private bool _escapeNonAscii;
 
         public JSONWriter()
         {
             
         }
 
-        private bool _escapeNonAscii;
-
         /// <summary>
-        /// 
-        /// Возвращает true если для объекта чтения json был задан текст для парсинга.
+        /// Возвращает true если был открыт объект для записи.
         /// </summary>
         private bool IsOpen()
         {
@@ -50,12 +49,17 @@ namespace OneScript.StandardLibrary.Json
         }
 
         /// <summary>
-        /// 
-        /// Возвращает true если для объекта чтения json был задан текст для парсинга.
+        /// Возвращает true если текст json выводится в строку.
         /// </summary>
         private bool IsOpenForString()
         {
             return _stringWriter != null;
+        }
+
+        private void CheckWriter()
+        {
+            if (!IsOpen())
+                throw NotOpenException();
         }
 
         private void SetDefaultOptions()
@@ -68,6 +72,12 @@ namespace OneScript.StandardLibrary.Json
 
         private void SetOptions(JSONWriterSettings settings)
         {
+            if (settings == null)
+            {
+                SetDefaultOptions();
+                return;
+            }
+
             _settings = settings;
             if (_settings.UseDoubleQuotes)
                 _writer.QuoteChar = '\"';
@@ -223,7 +233,6 @@ namespace OneScript.StandardLibrary.Json
         }
 
         /// <summary>
-        /// 
         /// Определяет текущие параметры записи JSON.
         /// </summary>
         /// <value>ПараметрыЗаписиJSON (JSONWriterSettings)</value>
@@ -234,9 +243,7 @@ namespace OneScript.StandardLibrary.Json
 
         }
 
-
         /// <summary>
-        /// 
         /// Показывает, будет ли проводиться проверка правильности структуры записываемого JSON объекта. В случае обнаружение ошибки, будет сгенерировано исключение. Например: при попытке записать значение без имени вне массива или записать окончание объекта без начала. Установка данного свойства не имеет немедленного эффекта. Установленное значение свойства будет использовано только после открытия файла или установки строки.
         /// После создания объекта данное свойство имеет значение Истина.
         /// </summary>
@@ -250,59 +257,39 @@ namespace OneScript.StandardLibrary.Json
 
 
         /// <summary>
-        /// 
-        /// Завершает запись текста JSON. Если производилась запись в файл, то файл закрывается.
-        /// Если производилась запись в строку, то результирующая строка будет получена в качестве возвращаемого значения метода.
-        /// Если производилась запись в файл, то метод вернет пустую строку.
+        /// Завершает запись текста JSON.
+        /// Если производилась запись в строку, то метод вернет результирующую строку.
+        /// Если производилась запись в файл, то файл закрывается, а метод вернет пустую строку.
         /// </summary>
-        ///
-        /// <returns name="String">
-        /// Значения данного типа содержат строку в формате Unicode произвольной длины.</returns>
+        /// <remarks>Допускается повторное закрытие. Будет возвращена пустая строка.</remarks>
+        /// <returns name="String">Строка в формате Unicode</returns>
         [ContextMethod("Закрыть", "Close")]
         public string Close()
         {
-            string res = "";
+            var result = IsOpenForString() ? _stringWriter.ToString() : String.Empty;
 
-            if (IsOpenForString())
-            {
-                res = _stringWriter.ToString();
-                _stringWriter.Close();
-                _stringWriter = null;
-            }
+            Dispose();
 
-            if (_writer != null)
-            {
-                _writer.Close();
-                _writer = null;
-            }
-
-            return res;
+            return result;
         }
 
-
         /// <summary>
-        /// 
         /// Выполняет запись произвольной строки в документ, при этом проверка структуры документа не выполняется.
         /// Если при использовании метода свойство ПроверятьСтруктуру установлено в значение Истина, то проверка структуры продолжается на следующем элементе.
         /// </summary>
-        ///
         /// <param name="stringValue">
         /// Строка, записываемая в документ JSON. </param>
         [ContextMethod("ЗаписатьБезОбработки", "WriteRaw")]
         public void WriteRaw(string stringValue)
         {
-            if (!IsOpen())
-                throw NotOpenException();
-
+            CheckWriter();
             _writer.WriteRaw(stringValue);
         }
 
 
         /// <summary>
-        /// 
         /// Записывает значение свойства JSON.
         /// </summary>
-        ///
         /// <param name="value">
         /// Записываемое значение. Типы: Строка (String), Число (Number), Булево (Boolean), Неопределено (Undefined) </param>
         /// <param name="useFormatWithExponent">
@@ -311,8 +298,7 @@ namespace OneScript.StandardLibrary.Json
         [ContextMethod("ЗаписатьЗначение", "WriteValue")]
         public void WriteValue(IValue value, bool useFormatWithExponent = false)
         {
-            if (!IsOpen())
-                throw NotOpenException();
+            CheckWriter();
 
             var clrValue = value.UnwrapToClrObject();
             switch (clrValue)
@@ -354,78 +340,70 @@ namespace OneScript.StandardLibrary.Json
         }
 
         /// <summary>
-        /// 
         /// Записывает имя свойства JSON.
         /// </summary>
-        ///
         /// <param name="propertyName">
         /// Имя свойства. </param>
         [ContextMethod("ЗаписатьИмяСвойства", "WritePropertyName")]
         public void WritePropertyName(string propertyName)
         {
+            CheckWriter();
             _writer.WritePropertyName(propertyName);
         }
 
 
         /// <summary>
-        /// 
         /// Записывает конец массива JSON.
         /// </summary>
-        ///
         [ContextMethod("ЗаписатьКонецМассива", "WriteEndArray")]
         public void WriteEndArray()
         {
+            CheckWriter();
             _writer.WriteEndArray();
         }
 
 
         /// <summary>
-        /// 
         /// Записывает конец объекта JSON.
         /// </summary>
-        ///
         [ContextMethod("ЗаписатьКонецОбъекта", "WriteEndObject")]
         public void WriteEndObject()
         {
+            CheckWriter();
             _writer.WriteEndObject();
         }
 
 
         /// <summary>
-        /// 
         /// Записывает начало массива JSON.
         /// </summary>
-        ///
         [ContextMethod("ЗаписатьНачалоМассива", "WriteStartArray")]
         public void WriteStartArray()
         {
+            CheckWriter();
             _writer.WriteStartArray();
         }
 
 
         /// <summary>
-        /// 
         /// Записывает начало объекта JSON.
         /// </summary>
-        ///
         [ContextMethod("ЗаписатьНачалоОбъекта", "WriteStartObject")]
         public void WriteStartObject()
         {
+            CheckWriter();
             _writer.WriteStartObject();
         }
 
 
         /// <summary>
-        /// 
         /// Открывает файл для записи JSON. Позволяет указать тип кодировки, который будет использован для записи файла JSON, а также использование BOM.
         /// </summary>
-        ///
         /// <param name="fileName">
         /// Имя файла, в который будет записываться текст JSON. </param>
         /// <param name="encoding">
         /// В качестве типа кодировки может быть указана одна из возможных кодировок текста. В этом случае файл будет записан в соответствующей кодировке. Если же в качестве параметра указана пустая строка или ничего не указано, то для записи файла будет использована кодировка UTF8.
         /// Поддерживаемые коды кодировок:
-        /// 
         /// Значение по умолчанию: UTF-8. </param>
         /// <param name="addBOM">
         /// Определяет, будет ли добавлен маркер порядка байт (BOM) к результирующему файлу JSON.
@@ -437,59 +415,106 @@ namespace OneScript.StandardLibrary.Json
         public void OpenFile(string fileName, string encoding = null, bool addBOM = false, JSONWriterSettings settings = null)
         {
             StreamWriter streamWriter;
-            
+            var textEncoding = TextEncodingEnum.GetEncodingByName(encoding ?? DEFAULT_ENCODING, addBOM);
+
             try
             {
-                if (encoding != null)
-                    streamWriter = FileOpener.OpenWriter(fileName, TextEncodingEnum.GetEncodingByName(encoding, addBOM));
-                else
-                    streamWriter = FileOpener.OpenWriter(fileName, TextEncodingEnum.GetEncodingByName("UTF-8", addBOM));
+               streamWriter = FileOpener.OpenWriter(fileName, textEncoding);
             }
             catch (Exception e)
             {
                 throw new RuntimeException(e.Message, e);
             }
 
-            if (IsOpen())
-                Close();
+            SetWriter(streamWriter, settings);
+        }
 
-            _writer = new JsonTextWriter(streamWriter);
-            if (settings == null)
-                SetDefaultOptions();
-            else
-                SetOptions(settings);
+        /// <summary>
+        /// Открывает поток для записи JSON. Позволяет указать тип кодировки, который будет использован для записи файла JSON, а также использование BOM.
+        /// </summary>
+        /// <param name="streamContext">
+        /// Поток, в который будет записываться текст JSON. </param>
+        /// <param name="encoding">
+        /// В качестве типа кодировки может быть указана одна из возможных кодировок текста. В этом случае файл будет записан в соответствующей кодировке. Если же в качестве параметра указана пустая строка или ничего не указано, то для записи файла будет использована кодировка UTF8.
+        /// Поддерживаемые коды кодировок:
+        /// Значение по умолчанию: UTF-8. </param>
+        /// <param name="addBOM">
+        /// Определяет, будет ли добавлен маркер порядка байт (BOM) к результирующему потоку JSON.
+        /// Внимание. Стандарт RFC7159 настоятельно рекомендует не добавлять маркер порядка байт (BOM) к документу JSON .
+        /// Значение по умолчанию: Ложь. </param>
+        /// <param name="settings">
+        /// Параметры, используемые при открытии потока для настройки записи в формате JSON.</param>
+        [ContextMethod("ОткрытьПоток", "OpenStream")]
+        public void OpenStream(IStreamWrapper streamContext, string encoding = null, bool addBOM = false, JSONWriterSettings settings = null)
+        {
+            if (streamContext is null)
+                throw new ArgumentNullException(nameof(streamContext));
 
-            SetNewLineChars(streamWriter);
+            if (streamContext.IsReadOnly)
+                throw CannotWriteException();
+
+            var textEncoding = TextEncodingEnum.GetEncodingByName(encoding ?? DEFAULT_ENCODING, addBOM);
+            StreamWriter streamWriter = new(streamContext.GetUnderlyingStream(), textEncoding);
+            SetWriter(streamWriter, settings);
         }
 
 
         /// <summary>
-        /// 
         /// Инициализирует объект для вывода результирующего JSON текста в строку.
         /// </summary>
-        ///
         /// <param name="settings">
         /// Параметры, используемые при записи объекта JSON.
         /// По умолчанию, содержит ПараметрыЗаписиJSON, сгенерированные автоматически. </param>
         [ContextMethod("УстановитьСтроку", "SetString")]
         public void SetString(JSONWriterSettings settings = null)
         {
+            _stringWriter = new StringWriter();
+            SetWriter(_stringWriter, settings);
+        }
+
+        private void SetWriter(TextWriter stream, JSONWriterSettings settings)
+        {
             if (IsOpen())
                 Close();
-            _stringWriter = new StringWriter();
-            _writer = new JsonTextWriter(_stringWriter);
-            if (settings == null)
-                SetDefaultOptions();
-            else
-                SetOptions(settings);
 
-            SetNewLineChars(_stringWriter);
+            _writer = new JsonTextWriter(stream);
+
+            SetOptions(settings);
+            SetNewLineChars(stream);
         }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && _writer is not null)
+            {
+                _stringWriter?.Dispose();
+                _stringWriter = null;
+
+                _writer.Close();
+                _writer = null;
+            }
+        }
+
+        #endregion
 
         RuntimeException NotOpenException()
         {
             return new RuntimeException(Locale.NStr
                 ("ru='Приемник данных JSON не открыт';en='JSON data target is not opened'"));
+        }
+
+        RuntimeException CannotWriteException()
+        {
+            return new("Попытка записи в поток не поддерживающий запись",
+                "Cannot write to a stream that does not support writing");
         }
 
         RuntimeException TypeNotSupportedException(Type type)
